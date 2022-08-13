d5597c0e21521ef198478a8d4c1e9669fe6e36e5
FongMi <b8892778@gmail.com>
2022-08-13 19:48:39 +0800

    Update Vod.java

--- app/src/main/java/com/fongmi/android/tv/bean/Vod.java
+++ app/src/main/java/com/fongmi/android/tv/bean/Vod.java
@@ -201,9 +201,7 @@ public class Vod {
             String[] urls = data.contains("#") ? data.split("#") : new String[]{data};
             for (String url : urls) {
                 String[] split = url.split("\\$");
-                Episode episode;
-                if (split.length >= 2) episode = new Vod.Flag.Episode(split[0], split[1]);
-                else episode = new Vod.Flag.Episode(ResUtil.getString(R.string.play), url);
+                Episode episode = split.length >= 2 ? new Vod.Flag.Episode(split[0], split[1]) : new Vod.Flag.Episode(ResUtil.getString(R.string.play), url);
                 if (!getEpisodes().contains(episode)) getEpisodes().add(episode);
             }
         }
