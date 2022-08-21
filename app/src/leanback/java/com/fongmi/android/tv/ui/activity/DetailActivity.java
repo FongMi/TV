4a2edd81d3430008a52d20477fa6f11b0a1c150d
FongMi <b8892778@gmail.com>
2022-08-21 21:40:51 +0800

    Fix auto next bug

--- app/src/leanback/java/com/fongmi/android/tv/ui/activity/DetailActivity.java
+++ app/src/leanback/java/com/fongmi/android/tv/ui/activity/DetailActivity.java
@@ -406,7 +406,7 @@ public class DetailActivity extends BaseActivity implements KeyDown.Listener {
             long duration = Players.get().getDuration();
             long current = Players.get().getCurrentPosition();
             if (mHistory.getOpening() >= current) Players.get().seekTo(mHistory.getOpening());
-            if (mHistory.getEnding() > 0 && mHistory.getEnding() + current >= duration) {
+            if (mHistory.getEnding() > 0 && duration > 0 && mHistory.getEnding() + current >= duration) {
                 keep = false;
                 onNext();
             }
