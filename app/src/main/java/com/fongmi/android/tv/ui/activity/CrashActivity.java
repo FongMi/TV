2b3d00c8a9b47f310fcb672886133395514ad469
FongMi <b8892778@gmail.com>
2022-08-25 09:42:12 +0800

    Add crash activity

---
+++ app/src/main/java/com/fongmi/android/tv/ui/activity/CrashActivity.java
@@ -0,0 +1,35 @@
+package com.fongmi.android.tv.ui.activity;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.viewbinding.ViewBinding;
+
+import com.fongmi.android.tv.R;
+import com.fongmi.android.tv.databinding.ActivityCrashBinding;
+
+import java.util.Objects;
+
+import cat.ereza.customactivityoncrash.CustomActivityOnCrash;
+
+public class CrashActivity extends BaseActivity {
+
+    private ActivityCrashBinding mBinding;
+
+    @Override
+    protected ViewBinding getBinding() {
+        return mBinding = ActivityCrashBinding.inflate(getLayoutInflater());
+    }
+
+    @Override
+    protected void initEvent() {
+        mBinding.details.setOnClickListener(v -> showError());
+        mBinding.restart.setOnClickListener(v -> CustomActivityOnCrash.restartApplication(this, Objects.requireNonNull(CustomActivityOnCrash.getConfigFromIntent(getIntent()))));
+    }
+
+    private void showError() {
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.crash_details_title)
+                .setMessage(CustomActivityOnCrash.getAllErrorDetailsFromIntent(this, getIntent()))
+                .setPositiveButton(R.string.crash_details_close, null)
+                .show();
+    }
+}
