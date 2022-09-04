092f2da69d161e22cbecd3d309efb27f67e213e1
FongMi <b8892778@gmail.com>
2022-09-04 14:42:37 +0800

    Adjust voice search

--- app/src/leanback/java/com/fongmi/android/tv/ui/custom/CustomEditText.java
+++ app/src/leanback/java/com/fongmi/android/tv/ui/custom/CustomEditText.java
@@ -1,13 +1,20 @@
 package com.fongmi.android.tv.ui.custom;
 
 import android.content.Context;
+import android.graphics.Rect;
 import android.util.AttributeSet;
+import android.view.animation.Animation;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatEditText;
 
+import com.fongmi.android.tv.R;
+import com.fongmi.android.tv.utils.ResUtil;
+
 public class CustomEditText extends AppCompatEditText {
+
+    private Animation flicker;
 
     public CustomEditText(@NonNull Context context) {
         super(context);
@@ -15,14 +22,18 @@ public class CustomEditText extends AppCompatEditText {
 
     public CustomEditText(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-    }
-
-    public CustomEditText(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
+        flicker = ResUtil.getAnim(R.anim.flicker);
     }
 
     @Override
     public boolean isFocused() {
         return true;
+    }
+
+    @Override
+    protected void onFocusChanged(boolean gainFocus, int direction, @Nullable Rect previouslyFocusedRect) {
+        super.onFocusChanged(gainFocus, direction, previouslyFocusedRect);
+        if (gainFocus) startAnimation(flicker);
+        else clearAnimation();
     }
 }
--- app/src/leanback/java/com/fongmi/android/tv/ui/custom/CustomMic.java
+++ app/src/leanback/java/com/fongmi/android/tv/ui/custom/CustomMic.java
@@ -1,7 +1,9 @@
 package com.fongmi.android.tv.ui.custom;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.PorterDuff;
 import android.graphics.Rect;
 import android.speech.RecognizerIntent;
@@ -10,9 +12,11 @@ import android.util.AttributeSet;
 import android.view.KeyEvent;
 import android.view.animation.Animation;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatImageView;
+import androidx.core.content.ContextCompat;
 
 import com.fongmi.android.tv.R;
 import com.fongmi.android.tv.utils.ResUtil;
@@ -21,6 +25,7 @@ import com.github.bassaer.library.MDColor;
 
 public class CustomMic extends AppCompatImageView {
 
+    private ActivityResultLauncher<String> launcher;
     private SpeechRecognizer recognizer;
     private Animation flicker;
     private boolean listen;
@@ -39,16 +44,26 @@ public class CustomMic extends AppCompatImageView {
         recognizer = SpeechRecognizer.createSpeechRecognizer(context);
     }
 
-    public boolean isListen() {
+    private boolean hasPermission() {
+        return ContextCompat.checkSelfPermission(getContext(), Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private boolean isListen() {
         return listen;
     }
 
-    public void setListen(boolean listen) {
+    private void setListen(boolean listen) {
         this.listen = listen;
     }
 
-    public void setListener(CustomListener listener) {
-        recognizer.setRecognitionListener(listener);
+    public void setListener(ActivityResultLauncher<String> launcher, CustomListener listener) {
+        this.recognizer.setRecognitionListener(listener);
+        this.launcher = launcher;
+    }
+
+    private void check() {
+        if (hasPermission()) start();
+        else launcher.launch(Manifest.permission.RECORD_AUDIO);
     }
 
     public void start() {
@@ -56,6 +71,7 @@ public class CustomMic extends AppCompatImageView {
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         setColorFilter(MDColor.RED_500, PorterDuff.Mode.SRC_IN);
         recognizer.startListening(intent);
+        startAnimation(flicker);
         setListen(true);
     }
 
@@ -70,7 +86,7 @@ public class CustomMic extends AppCompatImageView {
     @Override
     protected void onFocusChanged(boolean gainFocus, int direction, @Nullable Rect previouslyFocusedRect) {
         super.onFocusChanged(gainFocus, direction, previouslyFocusedRect);
-        if (gainFocus) startAnimation(flicker);
+        if (gainFocus) check();
         else stop();
     }
 
