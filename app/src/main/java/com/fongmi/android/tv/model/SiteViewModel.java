5e98ce67524a2f113396ba2e928fc7490b978acc
FongMi <b8892778@gmail.com>
2022-09-15 14:17:22 +0800

    Update SiteViewModel.java

--- app/src/main/java/com/fongmi/android/tv/model/SiteViewModel.java
+++ app/src/main/java/com/fongmi/android/tv/model/SiteViewModel.java
@@ -161,7 +161,7 @@ public class SiteViewModel extends ViewModel {
                 post(site, Result.fromJson(searchContent));
             } else {
                 HashMap<String, String> params = new HashMap<>();
-                if (site.getType() == 1) params.put("ac", "detail");
+                if (site.getType() != 0) params.put("ac", "detail");
                 params.put("wd", keyword);
                 String body = OKHttp.newCall(site.getApi(), params).execute().body().string();
                 SpiderDebug.log(site.getName() + "," + body);
