5e81ac533def873c460b113715c7559ead2844f4
FongMi <b8892778@gmail.com>
2022-09-21 18:34:20 +0800

    Clean code

--- app/src/mobile/java/com/fongmi/android/tv/ui/adapter/HistoryAdapter.java
+++ app/src/mobile/java/com/fongmi/android/tv/ui/adapter/HistoryAdapter.java
@@ -1,17 +1,17 @@
 package com.fongmi.android.tv.ui.adapter;
 
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.fongmi.android.tv.App;
 import com.fongmi.android.tv.R;
 import com.fongmi.android.tv.api.ApiConfig;
 import com.fongmi.android.tv.bean.History;
 import com.fongmi.android.tv.databinding.AdapterVodBinding;
-import com.fongmi.android.tv.utils.ImgUtil;
 import com.fongmi.android.tv.utils.ResUtil;
 
 import java.util.ArrayList;
@@ -22,7 +22,6 @@ public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.ViewHold
     private final OnClickListener mListener;
     private final List<History> mItems;
     private int width, height;
-    private boolean delete;
 
     public HistoryAdapter(OnClickListener listener) {
         this.mListener = listener;
@@ -34,47 +33,23 @@ public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.ViewHold
 
         void onItemClick(History item);
 
-        void onItemDelete(History item);
-
-        boolean onLongClick();
-    }
-
-    public boolean isDelete() {
-        return delete;
-    }
-
-    public void setDelete(boolean delete) {
-        this.delete = delete;
+        boolean onLongClick(History item);
     }
 
     private void setLayoutSize(int spanCount) {
-        int space = ResUtil.dp2px(32 + ((spanCount - 1) * 16));
+        int space = ResUtil.dp2px(32) + ResUtil.dp2px(16 * (spanCount - 1));
         int base = ResUtil.getScreenWidthPx() - space;
         width = base / spanCount;
         height = (int) (width / 0.75f);
     }
 
-    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener {
+    static class ViewHolder extends RecyclerView.ViewHolder {
 
         private final AdapterVodBinding binding;
 
         ViewHolder(@NonNull AdapterVodBinding binding) {
             super(binding.getRoot());
             this.binding = binding;
-            binding.getRoot().setOnClickListener(this);
-            binding.getRoot().setOnLongClickListener(this);
-        }
-
-        @Override
-        public void onClick(View view) {
-            History item = mItems.get(getLayoutPosition());
-            if (isDelete()) mListener.onItemDelete(item);
-            else mListener.onItemClick(item);
-        }
-
-        @Override
-        public boolean onLongClick(View v) {
-            return mListener.onLongClick();
         }
     }
 
@@ -104,10 +79,8 @@ public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.ViewHold
         holder.binding.name.setText(item.getVodName());
         holder.binding.site.setText(ApiConfig.getSiteName(item.getSiteKey()));
         holder.binding.remark.setText(ResUtil.getString(R.string.vod_last, item.getVodRemarks()));
-        holder.binding.name.setVisibility(delete ? View.GONE : View.VISIBLE);
-        holder.binding.site.setVisibility(delete ? View.GONE : View.VISIBLE);
-        holder.binding.remark.setVisibility(delete ? View.GONE : View.VISIBLE);
-        holder.binding.delete.setVisibility(!delete ? View.GONE : View.VISIBLE);
-        ImgUtil.load(item.getVodPic(), holder.binding.image);
+        holder.binding.getRoot().setOnClickListener(view -> mListener.onItemClick(item));
+        holder.binding.getRoot().setOnLongClickListener(view -> mListener.onLongClick(item));
+        Glide.with(App.get()).load(item.getVodPic()).centerCrop().error(R.drawable.ic_img_error).placeholder(R.drawable.ic_img_loading).into(holder.binding.image);
     }
 }
