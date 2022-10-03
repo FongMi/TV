e4a1d413692dcef76c28c76d8fa111b9ba9aa3f8
FongMi <b8892778@gmail.com>
2022-10-03 18:41:09 +0800

    Update OKHttp.java

--- app/src/main/java/com/fongmi/android/tv/net/OKHttp.java
+++ app/src/main/java/com/fongmi/android/tv/net/OKHttp.java
@@ -29,7 +29,7 @@ public class OKHttp {
     }
 
     private OkHttpClient.Builder getBuilder() {
-        return new OkHttpClient.Builder().readTimeout(5, TimeUnit.SECONDS).writeTimeout(5, TimeUnit.SECONDS).connectTimeout(5, TimeUnit.SECONDS).sslSocketFactory(new SSLSocketFactoryCompat(), SSLSocketFactoryCompat.trustAllCert);
+        return new OkHttpClient.Builder().readTimeout(15, TimeUnit.SECONDS).writeTimeout(15, TimeUnit.SECONDS).connectTimeout(5, TimeUnit.SECONDS).sslSocketFactory(new SSLSocketFactoryCompat(), SSLSocketFactoryCompat.trustAllCert);
     }
 
     private OkHttpClient client() {
