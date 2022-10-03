d1ea2edab371c7f58387b45c5bff17a5c2482237
FongMi <b8892778@gmail.com>
2022-10-04 00:08:01 +0800

    Add Migrations

--- app/src/main/java/com/fongmi/android/tv/db/AppDatabase.java
+++ app/src/main/java/com/fongmi/android/tv/db/AppDatabase.java
@@ -33,7 +33,7 @@ public abstract class AppDatabase extends RoomDatabase {
     }
 
     private static AppDatabase create(Context context) {
-        return Room.databaseBuilder(context, AppDatabase.class, "tv").addMigrations(MIGRATION_7_8).allowMainThreadQueries().fallbackToDestructiveMigration().build();
+        return Room.databaseBuilder(context, AppDatabase.class, "tv").addMigrations(MIGRATION_8_9).allowMainThreadQueries().fallbackToDestructiveMigration().build();
     }
 
     public abstract KeepDao getKeepDao();
@@ -44,10 +44,10 @@ public abstract class AppDatabase extends RoomDatabase {
 
     public abstract HistoryDao getHistoryDao();
 
-    static final Migration MIGRATION_7_8 = new Migration(7, 8) {
+    static final Migration MIGRATION_8_9 = new Migration(8, 9) {
         @Override
         public void migrate(@NonNull SupportSQLiteDatabase database) {
-            database.execSQL("ALTER TABLE Config ADD COLUMN json TEXT DEFAULT NULL");
+            database.execSQL("CREATE TABLE IF NOT EXISTS `Keep` (`key` TEXT NOT NULL, `siteName` TEXT, `vodName` TEXT, `vodPic` TEXT, `createTime` INTEGER NOT NULL, `cid` INTEGER NOT NULL, PRIMARY KEY(`key`))");
         }
     };
 }
