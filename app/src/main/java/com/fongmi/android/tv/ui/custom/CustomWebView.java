c10902fbbadc925da6da418e7a11046a459015e5
FongMi <b8892778@gmail.com>
2022-10-06 09:38:57 +0800

    Update CustomWebView.java

--- app/src/main/java/com/fongmi/android/tv/ui/custom/CustomWebView.java
+++ app/src/main/java/com/fongmi/android/tv/ui/custom/CustomWebView.java
@@ -90,6 +90,11 @@ public class CustomWebView extends WebView {
             public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
                 handler.proceed();
             }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return false;
+            }
         };
     }
 
