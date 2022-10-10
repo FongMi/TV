7cf4027281b6aa2a85ef17776777128f6b2cf68c
FongMi <b8892778@gmail.com>
2022-10-11 00:44:13 +0800

    Add log for url

--- app/src/main/java/com/fongmi/android/tv/ui/custom/CustomWebView.java
+++ app/src/main/java/com/fongmi/android/tv/ui/custom/CustomWebView.java
@@ -18,6 +18,7 @@ import androidx.annotation.Nullable;
 import com.fongmi.android.tv.api.ApiConfig;
 import com.fongmi.android.tv.player.ParseTask;
 import com.fongmi.android.tv.utils.Utils;
+import com.github.catvod.crawler.SpiderDebug;
 
 import java.io.ByteArrayInputStream;
 import java.util.Arrays;
@@ -113,6 +114,7 @@ public class CustomWebView extends WebView {
         handler.removeCallbacks(mTimer);
         handler.post(() -> {
             if (callback != null) callback.onParseSuccess(news, url, "");
+            SpiderDebug.log(url);
             stop(false);
         });
     }
