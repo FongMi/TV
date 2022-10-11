0b790def2c45d0975ea48b493ab0c7d6ec211e75
FongMi <b8892778@gmail.com>
2022-10-12 01:22:25 +0800

    init py

---
+++ pyramid/src/main/python/runner.py
@@ -0,0 +1,39 @@
+class Runner():
+    def __init__(self, spider):
+        self.spider = spider
+
+    def getDependence(self):
+        return self.spider.getDependence()
+
+    def getName(self):
+        return self.spider.getName()
+
+    def init(self, extend=""):
+        self.spider.init(extend)
+
+    def homeContent(self, filter):
+        return self.spider.homeContent(filter)
+
+    def homeVideoContent(self):
+        return self.spider.homeVideoContent()
+
+    def categoryContent(self, tid, pg, filter, extend):
+        return self.spider.categoryContent(tid, pg, filter, extend)
+
+    def detailContent(self, ids):
+        return self.spider.detailContent(ids)
+
+    def searchContent(self, key, quick):
+        return self.spider.searchContent(key, quick)
+
+    def playerContent(self, flag, id, vipFlags):
+        return self.spider.playerContent(flag, id, vipFlags)
+
+    def localProxy(self, param):
+        return self.spider.localProxy(param)
+
+    def isVideoFormat(self, url):
+        return self.spider.isVideoFormat(url)
+
+    def manualVideoCheck(self):
+        return self.spider.manualVideoCheck()
