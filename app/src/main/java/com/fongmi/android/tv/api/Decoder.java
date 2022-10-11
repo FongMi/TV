5d1810089491f8149b296851b2010dcdaa20d2c1
FongMi <b8892778@gmail.com>
2022-10-11 20:09:52 +0800

    Fix decode to hex

--- app/src/main/java/com/fongmi/android/tv/api/Decoder.java
+++ app/src/main/java/com/fongmi/android/tv/api/Decoder.java
@@ -64,9 +64,9 @@ public class Decoder {
     }
 
     private static byte[] decodeHex(String s) {
-        int len = s.length();
-        byte[] data = new byte[len / 2];
-        for (int i = 0; i < len; i += 2) data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4) + Character.digit(s.charAt(i + 1), 16));
+        int len = s.length() / 2;
+        byte[] data = new byte[len];
+        for (int i = 0; i < len; i++) data[i] = Integer.valueOf(s.substring(i * 2, i * 2 + 2), 16).byteValue();
         return data;
     }
 }
