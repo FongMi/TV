05a7fb3c2691a8641252adcdacba762c8b18d22c
FongMi <b8892778@gmail.com>
2022-10-13 01:37:11 +0800

    Add py

--- app/src/main/java/com/fongmi/android/tv/ui/custom/ProgressLayout.java
+++ app/src/main/java/com/fongmi/android/tv/ui/custom/ProgressLayout.java
@@ -114,9 +114,7 @@ public class ProgressLayout extends RelativeLayout {
     }
 
     private void showView(View view) {
-        view.setAlpha(0f);
         view.setVisibility(VISIBLE);
-        view.animate().alpha(1f).setDuration(250).setListener(null);
     }
 
     private void hideView(View view) {
