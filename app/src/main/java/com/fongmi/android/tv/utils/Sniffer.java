aea12e01fdcda0436591e8d23c2d4e8fbbffe33a
FongMi <b8892778@gmail.com>
2022-10-15 23:53:14 +0800

    Support m4a

--- app/src/main/java/com/fongmi/android/tv/utils/Sniffer.java
+++ app/src/main/java/com/fongmi/android/tv/utils/Sniffer.java
@@ -5,8 +5,8 @@ import java.util.regex.Pattern;
 public class Sniffer {
 
     public static final Pattern RULE = Pattern.compile(
-                    "http((?!http).){12,}?\\.(m3u8|mp4|flv|avi|mkv|rm|wmv|mpg)\\?.*|" +
-                    "http((?!http).){12,}\\.(m3u8|mp4|flv|avi|mkv|rm|wmv|mpg)|" +
+                    "http((?!http).){12,}?\\.(m3u8|mp4|flv|avi|mkv|rm|wmv|mpg|m4a)\\?.*|" +
+                    "http((?!http).){12,}\\.(m3u8|mp4|flv|avi|mkv|rm|wmv|mpg|m4a)|" +
                     "http((?!http).)*?xg.php\\?id=|" +
                     "http((?!http).)*?video/tos*"
     );
