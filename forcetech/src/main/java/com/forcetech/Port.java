779c9893e06b468d696add18a2226c63766f9c04
FongMi <b8892778@gmail.com>
2022-10-25 16:11:11 +0800

    Remove force

--- forcetech/src/main/java/com/forcetech/Port.java
+++ forcetech/src/main/java/com/forcetech/Port.java
@@ -1,20 +1,10 @@
 package com.forcetech;
 
-import android.net.Uri;
-
 public class Port {
 
-    public static int FORCE = 9001;
     public static int MTV = 9002;
 
     public static int get(String url) {
-        switch (Uri.parse(url).getScheme()) {
-            case "P2p":
-                return FORCE;
-            case "mitv":
-                return MTV;
-            default:
-                return -1;
-        }
+        return MTV;
     }
 }
