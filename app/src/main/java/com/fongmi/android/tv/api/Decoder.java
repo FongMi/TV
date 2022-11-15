68b85bf5b28eebd70ee86a2f2e8aa7bb7737e7a5
FongMi <b8892778@gmail.com>
2022-11-15 22:38:49 +0800

    Update Decoder.java

--- app/src/main/java/com/fongmi/android/tv/api/Decoder.java
+++ app/src/main/java/com/fongmi/android/tv/api/Decoder.java
@@ -2,15 +2,13 @@ package com.fongmi.android.tv.api;
 
 import android.util.Base64;
 
-import com.fongmi.android.tv.utils.UrlUtil;
-import com.github.catvod.net.OkHttp;
-import com.github.catvod.utils.Asset;
-import com.github.catvod.utils.Json;
-import com.github.catvod.utils.Path;
-import com.github.catvod.utils.Util;
+import com.fongmi.android.tv.net.OKHttp;
+import com.fongmi.android.tv.utils.FileUtil;
+import com.fongmi.android.tv.utils.Json;
+import com.google.common.io.BaseEncoding;
 
 import java.io.File;
-import java.nio.charset.StandardCharsets;
+import java.nio.charset.Charset;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -24,17 +22,11 @@ public class Decoder {
         String key = url.contains(";") ? url.split(";")[2] : "";
         url = url.contains(";") ? url.split(";")[0] : url;
         String data = getData(url);
+        if (Json.valid(data)) return data;
         if (data.isEmpty()) throw new Exception();
-        if (Json.valid(data)) return fix(url, data);
         if (data.contains("**")) data = base64(data);
         if (data.startsWith("2423")) data = cbc(data);
         if (key.length() > 0) data = ecb(data, key);
-        return fix(url, data);
-    }
-
-    private static String fix(String url, String data) {
-        if (url.startsWith("file") || url.startsWith("assets")) url = UrlUtil.convert(url);
-        data = data.replace("./", url.substring(0, url.lastIndexOf("/") + 1));
         return data;
     }
 
@@ -46,41 +38,46 @@ public class Decoder {
         }
     }
 
-    public static File getSpider(String url) {
+    public static File getSpider(String jar, String md5) {
         try {
-            File file = Path.jar(url);
-            String data = extract(getData(url.substring(4)));
-            return data.isEmpty() ? file : Path.write(file, Base64.decode(data, Base64.DEFAULT));
+            File file = FileUtil.getJar(jar);
+            if (md5.length() > 0 && FileUtil.equals(jar, md5)) return file;
+            String data = extract(getData(jar.substring(4)));
+            if (data.isEmpty()) return FileUtil.getJar(jar);
+            return FileUtil.write(file, Base64.decode(data, Base64.DEFAULT));
         } catch (Exception ignored) {
-            return Path.jar(url);
+            return FileUtil.getJar(jar);
         }
     }
 
-    private static String getData(String url) {
-        if (url.startsWith("file")) return Path.read(url);
-        if (url.startsWith("assets")) return Asset.read(url);
-        if (url.startsWith("http")) return OkHttp.string(url);
-        return "";
+    private static String getData(String url) throws Exception {
+        if (url.startsWith("http")) return OKHttp.newCall(url).execute().body().string();
+        else if (url.startsWith("file")) return FileUtil.read(url);
+        throw new Exception();
     }
 
     private static String ecb(String data, String key) throws Exception {
-        SecretKeySpec spec = new SecretKeySpec(padEnd(key).getBytes(), "AES");
+        SecretKeySpec spec = new SecretKeySpec(padEnd(key), "AES");
         Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
         cipher.init(Cipher.DECRYPT_MODE, spec);
-        return new String(cipher.doFinal(Util.hex2byte(data)), StandardCharsets.UTF_8);
+        return new String(cipher.doFinal(decodeHex(data)), Charset.forName("UTF-8"));
     }
 
     private static String cbc(String data) throws Exception {
-        String decode = new String(Util.hex2byte(data)).toLowerCase();
-        String key = padEnd(decode.substring(decode.indexOf("$#") + 2, decode.indexOf("#$")));
-        String iv = padEnd(decode.substring(decode.length() - 13));
-        SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), "AES");
-        IvParameterSpec ivSpec = new IvParameterSpec(iv.getBytes());
+        int indexKey = data.indexOf("2324") + 4;
+        String key = new String(decodeHex(data.substring(0, indexKey)), Charset.forName("UTF-8"));
+        key = key.replace("$#", "").replace("#$", "");
+        int indexIv = data.length() - 26;
+        String iv = data.substring(indexIv).trim();
+        iv = new String(decodeHex(iv), Charset.forName("UTF-8"));
+        SecretKeySpec keySpec = new SecretKeySpec(padEnd(key), "AES");
+        IvParameterSpec ivSpec = new IvParameterSpec(padEnd(iv));
         Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
         cipher.init(Cipher.DECRYPT_MODE, keySpec, ivSpec);
-        data = data.substring(data.indexOf("2324") + 4, data.length() - 26);
-        byte[] decryptData = cipher.doFinal(Util.hex2byte(data));
-        return new String(decryptData, StandardCharsets.UTF_8);
+        data = data.substring(indexKey, indexIv).trim();
+        byte[] encryptDataBytes = decodeHex(data);
+        byte[] decryptData = cipher.doFinal(encryptDataBytes);
+        return new String(decryptData, Charset.forName("UTF-8"));
     }
 
     private static String base64(String data) {
@@ -94,7 +91,11 @@ public class Decoder {
         return matcher.find() ? data.substring(data.indexOf(matcher.group()) + 10) : "";
     }
 
-    private static String padEnd(String key) {
-        return key + "0000000000000000".substring(key.length());
+    private static byte[] padEnd(String key) {
+        return (key + "0000000000000000".substring(key.length())).getBytes(Charset.forName("UTF-8"));
+    }
+
+    private static byte[] decodeHex(String s) {
+        return BaseEncoding.base16().decode(s.toUpperCase());
     }
 }
