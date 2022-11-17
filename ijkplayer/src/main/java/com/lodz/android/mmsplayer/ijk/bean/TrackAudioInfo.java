ec3eecd336e3cddd0743313e8782247cada0d0cd
FongMi <b8892778@gmail.com>
2022-11-17 18:43:55 +0800

    Support ijkplayer - part 5

---
+++ ijkplayer/src/main/java/com/lodz/android/mmsplayer/ijk/bean/TrackAudioInfo.java
@@ -0,0 +1,13 @@
+package com.lodz.android.mmsplayer.ijk.bean;
+
+public class TrackAudioInfo {
+
+    public String type = "";
+    public String language = "";
+    public String codec = "";
+    public String profileLevel = "";
+    public String sampleRate = "";
+    public String channels = "";
+    public String bitRate = "";
+    public boolean isSelected;
+}
