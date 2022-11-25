9b606aef8fe264406b6fda843d214f1c5ae68752
FongMi <b8892778@gmail.com>
2022-11-25 16:14:24 +0800

    Update IjkVideoView.java

--- ijkplayer/src/main/java/tv/danmaku/ijk/media/player/ui/IjkVideoView.java
+++ ijkplayer/src/main/java/tv/danmaku/ijk/media/player/ui/IjkVideoView.java
@@ -505,10 +505,9 @@ public class IjkVideoView extends FrameLayout implements MediaController.MediaPl
         mIjkPlayer.setOption(format, "dns_cache_timeout", -1);
         mIjkPlayer.setOption(format, "fflags", "fastseek");
         mIjkPlayer.setOption(format, "http-detect-range-support", 0);
-        mIjkPlayer.setOption(format, "timeout", 30 * 1000 * 1000);
         mIjkPlayer.setOption(player, "enable-accurate-seek", 0);
-        mIjkPlayer.setOption(player, "fast", 1);
         mIjkPlayer.setOption(player, "framedrop", 1);
+        mIjkPlayer.setOption(player, "max-buffer-size", 5242880);
         mIjkPlayer.setOption(player, "mediacodec", mCurrentDecode);
         mIjkPlayer.setOption(player, "mediacodec-auto-rotate", mCurrentDecode);
         mIjkPlayer.setOption(player, "mediacodec-handle-resolution-change", mCurrentDecode);
