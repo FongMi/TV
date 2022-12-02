8ce116381272285e3c3dd3e34ee2d864d442567f
FongMi <b8892778@gmail.com>
2022-12-02 09:20:12 +0800

    Update IjkVideoView.java

--- ijkplayer/src/main/java/tv/danmaku/ijk/media/player/ui/IjkVideoView.java
+++ ijkplayer/src/main/java/tv/danmaku/ijk/media/player/ui/IjkVideoView.java
@@ -6,6 +6,7 @@ import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Process;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.Gravity;
@@ -507,7 +508,7 @@ public class IjkVideoView extends FrameLayout implements MediaController.MediaPl
         mIjkPlayer.setOption(format, "http-detect-range-support", 0);
         mIjkPlayer.setOption(player, "enable-accurate-seek", 0);
         mIjkPlayer.setOption(player, "framedrop", 1);
-        mIjkPlayer.setOption(player, "max-buffer-size", 5242880);
+        mIjkPlayer.setOption(player, "max-buffer-size", 15 * 1024 * 1024);
         mIjkPlayer.setOption(player, "mediacodec", mCurrentDecode);
         mIjkPlayer.setOption(player, "mediacodec-auto-rotate", mCurrentDecode);
         mIjkPlayer.setOption(player, "mediacodec-handle-resolution-change", mCurrentDecode);
