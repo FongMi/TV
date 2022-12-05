710783f87aed68c2858244d7427ef96d59611408
FongMi <b8892778@gmail.com>
2022-12-05 16:52:35 +0800

    Support xwalk

--- app/src/main/java/com/fongmi/android/tv/ui/custom/CustomWebView.java
+++ app/src/main/java/com/fongmi/android/tv/ui/custom/CustomWebView.java
@@ -5,24 +5,31 @@ import android.content.Context;
 import android.net.http.SslError;
 import android.text.TextUtils;
 import android.webkit.CookieManager;
-import android.webkit.SslErrorHandler;
-import android.webkit.WebResourceResponse;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import android.webkit.ValueCallback;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 
 import com.fongmi.android.tv.App;
+import com.fongmi.android.tv.api.ApiConfig;
 import com.fongmi.android.tv.player.ParseTask;
 import com.fongmi.android.tv.utils.Utils;
 
+import org.xwalk.core.XWalkResourceClient;
+import org.xwalk.core.XWalkView;
+import org.xwalk.core.XWalkWebResourceRequest;
+import org.xwalk.core.XWalkWebResourceResponse;
+
+import java.io.ByteArrayInputStream;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public class CustomWebView extends WebView {
+public class CustomWebView extends XWalkView {
 
     private ParseTask.Callback callback;
+    private List<String> keys;
+    private String ads;
     private int retry;
 
     public CustomWebView(@NonNull Context context) {
@@ -32,13 +39,15 @@ public class CustomWebView extends WebView {
 
     @SuppressLint("SetJavaScriptEnabled")
     public void initSettings() {
+        this.ads = ApiConfig.get().getAds();
+        this.keys = Arrays.asList("user-agent", "referer", "origin");
         getSettings().setUseWideViewPort(true);
         getSettings().setDatabaseEnabled(true);
         getSettings().setDomStorageEnabled(true);
         getSettings().setJavaScriptEnabled(true);
         getSettings().setLoadWithOverviewMode(true);
         getSettings().setJavaScriptCanOpenWindowsAutomatically(false);
-        setWebViewClient(webViewClient());
+        setResourceClient(webViewClient());
     }
 
     private void setUserAgent(Map<String, String> headers) {
@@ -57,23 +66,26 @@ public class CustomWebView extends WebView {
         retry = 0;
     }
 
-    private WebViewClient webViewClient() {
-        return new WebViewClient() {
-            @Nullable
+    private XWalkResourceClient webViewClient() {
+        return new XWalkResourceClient(this) {
             @Override
-            public WebResourceResponse shouldInterceptRequest(WebView view, String url) {
+            public XWalkWebResourceResponse shouldInterceptLoadRequest(XWalkView view, XWalkWebResourceRequest request) {
+                String url = request.getUrl().toString();
+                String host = request.getUrl().getHost();
+                if (ads.contains(host)) return createXWalkWebResourceResponse("text/plain", "utf-8", new ByteArrayInputStream("".getBytes()));
                 App.post(mTimer, 15 * 1000);
-                if (Utils.isVideoFormat(url)) post(url);
-                return super.shouldInterceptRequest(view, url);
+                Map<String, String> headers = request.getRequestHeaders();
+                if (Utils.isVideoFormat(url, headers)) post(headers, url);
+                return super.shouldInterceptLoadRequest(view, request);
             }
 
             @Override
-            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
-                handler.proceed();
+            public void onReceivedSslError(XWalkView view, ValueCallback<Boolean> callback, SslError error) {
+                callback.onReceiveValue(true);
             }
 
             @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            public boolean shouldOverrideUrlLoading(XWalkView view, String url) {
                 return false;
             }
         };
@@ -84,14 +96,15 @@ public class CustomWebView extends WebView {
         public void run() {
             if (retry > 3) return;
             if (retry++ == 3) stop(true);
-            else reload();
+            else reload(RELOAD_NORMAL);
         }
     };
 
-    private void post(String url) {
+    private void post(Map<String, String> headers, String url) {
         Map<String, String> news = new HashMap<>();
         String cookie = CookieManager.getInstance().getCookie(url);
         if (!TextUtils.isEmpty(cookie)) news.put("cookie", cookie);
+        for (String key : headers.keySet()) if (keys.contains(key.toLowerCase())) news.put(key, headers.get(key));
         App.removeCallbacks(mTimer);
         App.post(() -> {
             if (callback != null) callback.onParseSuccess(news, url, "");
