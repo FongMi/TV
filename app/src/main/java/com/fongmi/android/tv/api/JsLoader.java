19bbdb85a17580be4f476dcd8ecf708a470c7fa0
FongMi <b8892778@gmail.com>
2022-12-07 18:19:56 +0800

    Support drpy - part 1

--- app/src/main/java/com/fongmi/android/tv/api/JsLoader.java
+++ app/src/main/java/com/fongmi/android/tv/api/JsLoader.java
@@ -1,12 +1,18 @@
 package com.fongmi.android.tv.api;
 
-import com.github.catvod.crawler.Spider;
+import android.content.Context;
 
+import com.fongmi.android.tv.App;
+import com.github.catvod.crawler.Spider;
+import com.github.catvod.crawler.SpiderNull;
+
+import java.lang.reflect.Method;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class JsLoader {
 
     private final ConcurrentHashMap<String, Spider> spiders;
+    private Object loader;
 
     public JsLoader() {
         spiders = new ConcurrentHashMap<>();
@@ -18,6 +24,24 @@ public class JsLoader {
     }
 
     private void init() {
+        try {
+            loader = Class.forName("com.hiker.drpy.Loader").newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
+    public Spider getSpider(String key, String api, String ext) {
+        try {
+            if (spiders.containsKey(key)) return spiders.get(key);
+            Method method = loader.getClass().getMethod("spider", Context.class, String.class, String.class);
+            Spider spider = (Spider) method.invoke(loader, App.get(), api, ext);
+            spider.init(App.get(), ext);
+            spiders.put(key, spider);
+            return spider;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new SpiderNull();
+        }
     }
 }
