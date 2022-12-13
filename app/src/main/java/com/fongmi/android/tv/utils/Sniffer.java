39c31377e21640ee7aea4b776926b841680710d3
FongMi <b8892778@gmail.com>
2022-12-13 18:48:37 +0800

    Update rule

--- app/src/main/java/com/fongmi/android/tv/utils/Sniffer.java
+++ app/src/main/java/com/fongmi/android/tv/utils/Sniffer.java
@@ -5,8 +5,8 @@ import java.util.regex.Pattern;
 public class Sniffer {
 
     public static final Pattern RULE = Pattern.compile(
-                    "http((?!http).){12,}?\\.(m3u8|mp4|flv|avi|mkv|rm|wmv|mpg|m4a)\\?.*|" +
-                    "http((?!http).){12,}\\.(m3u8|mp4|flv|avi|mkv|rm|wmv|mpg|m4a)|" +
+                    "http((?!http).){12,}?\\.(m3u8|mp4|flv|avi|mkv|rm|wmv|mpg|m4a|mp3)\\?.*|" +
+                    "http((?!http).){12,}\\.(m3u8|mp4|flv|avi|mkv|rm|wmv|mpg|m4a|mp3)|" +
                     "http((?!http).)*?video/tos*"
     );
 
