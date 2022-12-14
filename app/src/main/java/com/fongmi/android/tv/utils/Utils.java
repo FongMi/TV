528cdbc4f36fc9abdbd61f74553e96129429e8e1
FongMi <b8892778@gmail.com>
2022-12-14 18:06:03 +0800

    Update

--- app/src/main/java/com/fongmi/android/tv/utils/Utils.java
+++ app/src/main/java/com/fongmi/android/tv/utils/Utils.java
@@ -1,6 +1,5 @@
 package com.fongmi.android.tv.utils;
 
-import android.app.Activity;
 import android.content.Context;
 import android.net.Uri;
 import android.os.IBinder;
@@ -56,11 +55,6 @@ public class Utils {
 
     public static boolean isMenuKey(KeyEvent event) {
         return event.getAction() == KeyEvent.ACTION_UP && event.getKeyCode() == KeyEvent.KEYCODE_MENU;
-    }
-
-    public static void hideSystemUI(Activity activity) {
-        int flags = View.SYSTEM_UI_FLAG_LOW_PROFILE | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
-        activity.getWindow().getDecorView().setSystemUiVisibility(flags);
     }
 
     public static boolean isVideoFormat(String url) {
