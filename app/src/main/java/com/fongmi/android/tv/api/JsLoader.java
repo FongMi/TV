5e15910bc9e497959eac4cfbc1196c99943ede55
FongMi <b8892778@gmail.com>
2022-12-14 14:01:39 +0800

    Update js library

--- app/src/main/java/com/fongmi/android/tv/api/JsLoader.java
+++ app/src/main/java/com/fongmi/android/tv/api/JsLoader.java
@@ -31,7 +31,7 @@ public class JsLoader {
     public Spider getSpider(String key, String api, String ext) {
         try {
             if (spiders.containsKey(key)) return spiders.get(key);
-            Spider spider = new Spider(api, ext);
+            Spider spider = new Spider(api);
             spider.init(App.get(), ext);
             spiders.put(key, spider);
             return spider;
