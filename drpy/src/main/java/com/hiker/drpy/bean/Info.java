2bfbe9bf52ff5e16967ff757db56104aeb311a20
FongMi <b8892778@gmail.com>
2022-12-15 16:59:20 +0800

    Fix some bug

--- drpy/src/main/java/com/hiker/drpy/bean/Info.java
+++ drpy/src/main/java/com/hiker/drpy/bean/Info.java
@@ -23,23 +23,19 @@ public class Info {
             String[] rules = rule.split("--");
             setExcludes(rules);
             setRule(rules[0]);
-            setIndex(pos);
         } else if (pos.contains("--")) {
             String[] rules = pos.split("--");
             setExcludes(rules);
-            setIndex(rules[0]);
+            pos = rules[0];
+        }
+        try {
+            index = Integer.parseInt(pos.split("\\(")[1].split("\\)")[0]);
+        } catch (Exception ignored) {
         }
     }
 
     public void setExcludes(String[] rules) {
         excludes = new ArrayList<>(Arrays.asList(rules));
         excludes.remove(0);
-    }
-
-    public void setIndex(String pos) {
-        try {
-            index = Integer.parseInt(pos.split("\\(")[1].split("\\)")[0]);
-        } catch (Exception ignored) {
-        }
     }
 }
