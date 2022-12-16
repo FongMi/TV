db9f2c95ce931988cef28f3f3ebf22041dea9aac
FongMi <b8892778@gmail.com>
2022-12-16 18:41:07 +0800

    Update dev

--- ijkplayer/src/main/java/tv/danmaku/ijk/media/player/ui/IjkVideoView.java
+++ ijkplayer/src/main/java/tv/danmaku/ijk/media/player/ui/IjkVideoView.java
@@ -507,16 +507,19 @@ public class IjkVideoView extends FrameLayout implements MediaController.MediaPl
         mIjkPlayer.setOption(format, "http-detect-range-support", 0);
         mIjkPlayer.setOption(player, "enable-accurate-seek", 0);
         mIjkPlayer.setOption(player, "framedrop", 1);
-        mIjkPlayer.setOption(player, "max-buffer-size", 500 * 1024 * 1024);
+        mIjkPlayer.setOption(player, "max-fps", 120);
+        mIjkPlayer.setOption(player, "max-buffer-size", 512 * 1024 * 1024);
         mIjkPlayer.setOption(player, "mediacodec", mCurrentDecode);
         mIjkPlayer.setOption(player, "mediacodec-auto-rotate", mCurrentDecode);
         mIjkPlayer.setOption(player, "mediacodec-handle-resolution-change", mCurrentDecode);
         mIjkPlayer.setOption(player, "mediacodec-hevc", mCurrentDecode);
         mIjkPlayer.setOption(player, "opensles", 0);
         mIjkPlayer.setOption(player, "overlay-format", IjkMediaPlayer.SDL_FCC_RV32);
+        mIjkPlayer.setOption(player, "render-wait-start", 1);
         mIjkPlayer.setOption(player, "reconnect", 1);
         mIjkPlayer.setOption(player, "soundtouch", 1);
         mIjkPlayer.setOption(player, "start-on-prepared", 1);
+        mIjkPlayer.setOption(player, "sync-av-start", 1);
         mIjkPlayer.setOption(player, "subtitle", 1);
         if (mUri.getScheme() != null && mUri.getScheme().startsWith("rtsp")) {
             mIjkPlayer.setOption(format, "infbuf", 1);
