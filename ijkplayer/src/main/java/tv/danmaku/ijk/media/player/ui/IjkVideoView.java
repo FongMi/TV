889ac13dcb88cd7c6652f096ab3cca128a69ffde
FongMi <b8892778@gmail.com>
2022-12-16 12:05:28 +0800

    Update IjkVideoView.java

--- ijkplayer/src/main/java/tv/danmaku/ijk/media/player/ui/IjkVideoView.java
+++ ijkplayer/src/main/java/tv/danmaku/ijk/media/player/ui/IjkVideoView.java
@@ -145,7 +145,7 @@ public class IjkVideoView extends FrameLayout implements MediaController.MediaPl
     }
 
     public void setMediaSource(String path, Map<String, String> headers) {
-        setVideoURI(Uri.parse(path), headers);
+        setVideoURI(Uri.parse(path.trim()), headers);
     }
 
     public void setVideoURI(Uri uri, Map<String, String> headers) {
