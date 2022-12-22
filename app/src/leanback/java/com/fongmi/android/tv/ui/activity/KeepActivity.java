38c717d6b303e7fb7f35c49fa8f47918a97c2e4b
FongMi <b8892778@gmail.com>
2022-12-22 18:07:59 +0800

    Clean code

--- app/src/leanback/java/com/fongmi/android/tv/ui/activity/KeepActivity.java
+++ app/src/leanback/java/com/fongmi/android/tv/ui/activity/KeepActivity.java
@@ -51,6 +51,21 @@ public class KeepActivity extends BaseActivity implements KeepAdapter.OnClickLis
         mAdapter.addAll(Keep.getVod());
     }
 
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onRefreshEvent(RefreshEvent event) {
+        if (event.getType() == RefreshEvent.Type.KEEP) getKeep();
+    }
+
+    @Override
+    public void onItemClick(Keep item) {
+        Config config = Config.find(item.getCid());
+        if (item.getCid() == ApiConfig.getCid()) {
+            DetailActivity.start(this, item.getSiteKey(), item.getVodId());
+        } else {
+            loadConfig(config, item);
+        }
+    }
+
     private void loadConfig(Config config, Keep item) {
         ApiConfig.get().clear().config(config).load(true, new Callback() {
             @Override
@@ -65,23 +80,6 @@ public class KeepActivity extends BaseActivity implements KeepAdapter.OnClickLis
                 CollectActivity.start(getActivity(), item.getVodName());
             }
         });
-    }
-
-    @Subscribe(threadMode = ThreadMode.MAIN)
-    public void onRefreshEvent(RefreshEvent event) {
-        if (event.getType() == RefreshEvent.Type.KEEP) getKeep();
-    }
-
-    @Override
-    public void onItemClick(Keep item) {
-        Config config = Config.find(item.getCid());
-        if (item.getCid() == ApiConfig.getCid()) {
-            DetailActivity.start(this, item.getSiteKey(), item.getVodId());
-        } else if (config == null) {
-            CollectActivity.start(this, item.getVodName());
-        } else {
-            loadConfig(config, item);
-        }
     }
 
     @Override
