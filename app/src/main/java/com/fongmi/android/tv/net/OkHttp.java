1ceaa80e06f6a2da6b523f0837a1c557645d48d6
FongMi <b8892778@gmail.com>
2022-12-30 18:05:56 +0800

    Fix build

---
+++ app/src/main/java/com/fongmi/android/tv/net/OkHttp.java
@@ -0,0 +1,60 @@
+package com.fongmi.android.tv.net;
+
+import com.fongmi.android.tv.App;
+import com.google.net.cronet.okhttptransport.CronetInterceptor;
+
+import org.chromium.net.CronetEngine;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Call;
+import okhttp3.Headers;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+public class OkHttp {
+
+    private final OkHttpClient mOk;
+
+    private static class Loader {
+        static volatile OkHttp INSTANCE = new OkHttp();
+    }
+
+    public static OkHttp get() {
+        return Loader.INSTANCE;
+    }
+
+    public OkHttp() {
+        mOk = getBuilder().build();
+    }
+
+    private OkHttpClient.Builder getBuilder() {
+        return new OkHttpClient.Builder().addInterceptor(CronetInterceptor.newBuilder(new CronetEngine.Builder(App.get()).build()).build()).callTimeout(30, TimeUnit.SECONDS).readTimeout(30, TimeUnit.SECONDS).writeTimeout(30, TimeUnit.SECONDS).connectTimeout(30, TimeUnit.SECONDS).hostnameVerifier(SSLSocketFactoryCompat.hostnameVerifier).sslSocketFactory(new SSLSocketFactoryCompat(), SSLSocketFactoryCompat.trustAllCert);
+    }
+
+    private OkHttpClient client() {
+        return mOk;
+    }
+
+    public static Call newCall(String url) {
+        return get().client().newCall(new Request.Builder().url(url).build());
+    }
+
+    public static Call newCall(String url, Headers headers) {
+        return get().client().newCall(new Request.Builder().url(url).headers(headers).build());
+    }
+
+    public static Call newCall(String url, LinkedHashMap<String, String> params) {
+        return get().client().newCall(new Request.Builder().url(buildUrl(url, params)).build());
+    }
+
+    private static HttpUrl buildUrl(String url, LinkedHashMap<String, String> params) {
+        HttpUrl.Builder builder = Objects.requireNonNull(HttpUrl.parse(url)).newBuilder();
+        for (Map.Entry<String, String> entry : params.entrySet()) builder.addQueryParameter(entry.getKey(), entry.getValue());
+        return builder.build();
+    }
+}
