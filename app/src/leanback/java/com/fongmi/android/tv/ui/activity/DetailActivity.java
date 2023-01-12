cdc281229fea025b130732c3757ee7a6038c1247
FongMi <b8892778@gmail.com>
2023-01-13 01:00:03 +0800

    Fix seek ui bug

--- app/src/leanback/java/com/fongmi/android/tv/ui/activity/DetailActivity.java
+++ app/src/leanback/java/com/fongmi/android/tv/ui/activity/DetailActivity.java
@@ -840,15 +840,15 @@ public class DetailActivity extends BaseActivity implements CustomKeyDownVod.Lis
 
     @Override
     public void onSeeking(int time) {
+        mBinding.widget.center.setVisibility(View.VISIBLE);
         mBinding.widget.exoDuration.setText(mPlayers.getDurationTime());
         mBinding.widget.exoPosition.setText(mPlayers.getPositionTime(time));
         mBinding.widget.action.setImageResource(time > 0 ? R.drawable.ic_forward : R.drawable.ic_rewind);
-        mBinding.widget.center.setVisibility(View.VISIBLE);
     }
 
     @Override
     public void onSeekTo(int time) {
-        mPlayers.seekTo(time);
+        App.post(() -> mPlayers.seekTo(time),500);
         mKeyDown.resetTime();
         onPlay(500);
     }
