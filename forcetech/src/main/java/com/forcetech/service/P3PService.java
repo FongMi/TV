73d1d0fe4eb1527dbad15b9ccc74d152a870bafb
FongMi <b8892778@gmail.com>
2023-01-27 23:54:05 +0800

    Clean code

--- forcetech/src/main/java/com/forcetech/service/P3PService.java
+++ forcetech/src/main/java/com/forcetech/service/P3PService.java
@@ -1,34 +1,11 @@
 package com.forcetech.service;
 
-import android.app.Service;
-import android.content.Intent;
-import android.os.IBinder;
-
 import com.forcetech.Util;
-import com.forcetech.android.ForceTV;
-import com.gsoft.mitv.LocalBinder;
 
-public class P3PService extends Service {
-
-    private ForceTV forceTV;
-    private IBinder binder;
+public class P3PService extends PxPService {
 
     @Override
-    public void onCreate() {
-        super.onCreate();
-        binder = new LocalBinder();
-    }
-
-    @Override
-    public IBinder onBind(Intent intent) {
-        forceTV = new ForceTV();
-        forceTV.start(intent.getStringExtra("path"), Util.P3P);
-        return binder;
-    }
-
-    @Override
-    public boolean onUnbind(Intent intent) {
-        if (forceTV != null) forceTV.stop();
-        return super.onUnbind(intent);
+    public int getPort() {
+        return Util.P3P;
     }
 }
---
+++ forcetech/src/main/java/com/forcetech/service/PxPService.java
@@ -0,0 +1,35 @@
+package com.forcetech.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import com.forcetech.android.ForceTV;
+import com.gsoft.mitv.LocalBinder;
+
+public abstract class PxPService extends Service {
+
+    private ForceTV forceTV;
+    private IBinder binder;
+
+    public abstract int getPort();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        binder = new LocalBinder();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        forceTV = new ForceTV();
+        forceTV.start(intent.getStringExtra("path"), getPort());
+        return binder;
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        if (forceTV != null) forceTV.stop();
+        return super.onUnbind(intent);
+    }
+}
