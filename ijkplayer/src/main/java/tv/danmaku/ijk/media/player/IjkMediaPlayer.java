306ccc58192919f614f11185d3db943ea9478fdf
FongMi <b8892778@gmail.com>
2023-02-01 15:04:50 +0800

    Update IjkMediaPlayer.java

--- ijkplayer/src/main/java/tv/danmaku/ijk/media/player/IjkMediaPlayer.java
+++ ijkplayer/src/main/java/tv/danmaku/ijk/media/player/IjkMediaPlayer.java
@@ -187,8 +187,8 @@ public final class IjkMediaPlayer extends AbstractMediaPlayer {
             if (!mIsLibLoaded) {
                 if (libLoader == null) libLoader = sLocalLibLoader;
                 libLoader.loadLibrary("ijkffmpeg");
-                libLoader.loadLibrary("ijksdl");
                 libLoader.loadLibrary("ijkplayer");
+                libLoader.loadLibrary("ijksdl");
                 mIsLibLoaded = true;
             }
         }
