ba566579f25ef62b7f90ac43a8407741eb3b1924
FongMi <b8892778@gmail.com>
2023-02-20 12:42:44 +0800

    [mobile] adjust ui

--- app/src/main/java/com/fongmi/android/tv/utils/Utils.java
+++ app/src/main/java/com/fongmi/android/tv/utils/Utils.java
@@ -10,7 +10,6 @@ import android.text.TextUtils;
 import android.util.Base64;
 import android.view.KeyEvent;
 import android.view.View;
-import android.view.Window;
 import android.view.inputmethod.InputMethodManager;
 
 import androidx.fragment.app.FragmentActivity;
@@ -60,13 +59,19 @@ public class Utils {
         return event.getAction() == KeyEvent.ACTION_UP && event.getKeyCode() == KeyEvent.KEYCODE_MENU;
     }
 
-    public static void hideSystemUI(Activity activity) {
-        hideSystemUI(activity.getWindow());
+    public static void toggleFullscreen(Activity activity, boolean fullscreen) {
+        if (fullscreen) Utils.hideSystemUI(activity);
+        else Utils.showSystemUI(activity);
     }
 
-    public static void hideSystemUI(Window window) {
+    public static void showSystemUI(Activity activity) {
+        int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
+        activity.getWindow().getDecorView().setSystemUiVisibility(flags);
+    }
+
+    public static void hideSystemUI(Activity activity) {
         int flags = View.SYSTEM_UI_FLAG_LOW_PROFILE | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
-        window.getDecorView().setSystemUiVisibility(flags);
+        activity.getWindow().getDecorView().setSystemUiVisibility(flags);
     }
 
     public static boolean isVideoFormat(String url) {
