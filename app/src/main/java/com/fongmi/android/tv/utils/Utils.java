4dc6c076e1670efdc1b3ee63a097509442164ce9
FongMi <b8892778@gmail.com>
2023-02-20 14:39:50 +0800

    [mobile] support pip mode

--- app/src/main/java/com/fongmi/android/tv/utils/Utils.java
+++ app/src/main/java/com/fongmi/android/tv/utils/Utils.java
@@ -2,12 +2,16 @@ package com.fongmi.android.tv.utils;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.PictureInPictureParams;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.IBinder;
 import android.provider.Settings;
 import android.text.TextUtils;
 import android.util.Base64;
+import android.util.Rational;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -72,6 +76,21 @@ public class Utils {
     public static void hideSystemUI(Activity activity) {
         int flags = View.SYSTEM_UI_FLAG_LOW_PROFILE | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
         activity.getWindow().getDecorView().setSystemUiVisibility(flags);
+    }
+
+    public static boolean hasPIP() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.O && App.get().getPackageManager().hasSystemFeature(PackageManager.FEATURE_PICTURE_IN_PICTURE);
+    }
+
+    public static void enterPIP(Activity activity) {
+        try {
+            if (!hasPIP() || activity.isInPictureInPictureMode()) return;
+            PictureInPictureParams.Builder builder = new PictureInPictureParams.Builder();
+            builder.setAspectRatio(new Rational(16, 9)).build();
+            activity.enterPictureInPictureMode(builder.build());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static boolean isVideoFormat(String url) {
