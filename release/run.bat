fd96a9a80241318b11027e3e94547b33fd55c3ef
FongMi <b8892778@gmail.com>
2023-02-20 19:11:53 +0800

    [mobile] Update to 1.0.1

--- release/run.bat
+++ release/run.bat
@@ -1,4 +1,8 @@
 move .\leanbackJava\release\*.apk .\
 move .\leanbackPython\release\*.apk .\
+move .\mobileJava\release\*.apk .\
+move .\mobilePython\release\*.apk .\
 rmdir /s /q .\leanbackJava
 rmdir /s /q .\leanbackPython
+rmdir /s /q .\mobileJava
+rmdir /s /q .\mobilePython
