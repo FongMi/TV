f637dac8ee184be6167b11716b675b5a974df644
FongMi <b8892778@gmail.com>
2023-02-21 14:28:12 +0800

    [mobile] fix parse layout

--- app/src/main/java/com/fongmi/android/tv/utils/Utils.java
+++ app/src/main/java/com/fongmi/android/tv/utils/Utils.java
@@ -70,8 +70,7 @@ public class Utils {
     }
 
     public static void showSystemUI(Activity activity) {
-        int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
-        activity.getWindow().getDecorView().setSystemUiVisibility(flags);
+        activity.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
     }
 
     public static void hideSystemUI(Activity activity) {
