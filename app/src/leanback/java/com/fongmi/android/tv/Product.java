5f44be46a967825cac53df1f232e1b3bfe20208d
FongMi <b8892778@gmail.com>
2023-02-22 11:57:21 +0800

    [mobile] support autosize

--- app/src/leanback/java/com/fongmi/android/tv/Product.java
+++ app/src/leanback/java/com/fongmi/android/tv/Product.java
@@ -9,20 +9,15 @@ import me.jessyan.autosize.AutoSizeCompat;
 
 public class Product {
 
-    public static Resources hackResources(Resources resources) {
-        try {
-            AutoSizeCompat.autoConvertDensityOfGlobal(resources);
-            return resources;
-        } catch (Exception ignored) {
-            return resources;
-        }
-    }
-
     public static int getColumn() {
         return Math.abs(Prefers.getSize() - 7);
     }
 
     public static void bootLive() {
         LiveActivity.start(App.activity());
+    }
+
+    public static int getSizeInDp(boolean land) {
+        return land ? AutoSizeConfig.getInstance().getDesignWidthInDp() : AutoSizeConfig.getInstance().getDesignHeightInDp();
     }
 }
