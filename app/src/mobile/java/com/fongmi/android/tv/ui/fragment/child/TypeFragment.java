e8914ccbd2a5923f799b80ebb9dbdbb7fb2ec387
FongMi <b8892778@gmail.com>
2023-02-28 01:48:57 +0800

    [mobile] fix vod size

--- app/src/mobile/java/com/fongmi/android/tv/ui/fragment/child/TypeFragment.java
+++ app/src/mobile/java/com/fongmi/android/tv/ui/fragment/child/TypeFragment.java
@@ -88,6 +88,7 @@ public class TypeFragment extends BaseFragment implements CustomScroller.Callbac
         mBinding.recycler.setHasFixedSize(true);
         mBinding.recycler.setAdapter(mVodAdapter = new VodAdapter(this));
         mBinding.recycler.setLayoutManager(new GridLayoutManager(getContext(), Product.getColumn()));
+        mVodAdapter.setSize(Product.getSpec(getActivity()));
     }
 
     private void setViewModel() {
