c939d7f3383d384ff1fd5a2e2cd9db5e1cb7302e
FongMi <b8892778@gmail.com>
2023-03-13 18:38:27 +0800

    [mobile] fix fab show hide

--- app/src/main/java/com/fongmi/android/tv/utils/Utils.java
+++ app/src/main/java/com/fongmi/android/tv/utils/Utils.java
@@ -22,7 +22,6 @@ import androidx.fragment.app.FragmentActivity;
 
 import com.fongmi.android.tv.App;
 import com.fongmi.android.tv.server.Server;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.permissionx.guolindev.PermissionX;
 
 import java.math.BigInteger;
@@ -82,26 +81,6 @@ public class Utils {
     public static void hideSystemUI(Window window) {
         int flags = View.SYSTEM_UI_FLAG_LOW_PROFILE | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
         window.getDecorView().setSystemUiVisibility(flags);
-    }
-
-    public static void toggleFab(int dy, FloatingActionButton fab) {
-        if (Math.abs(dy) < 50) return;
-        if (dy > 0) Utils.hideFab(fab);
-        else Utils.showFab(fab);
-    }
-
-    public static void showFab(FloatingActionButton fab) {
-        if (fab.getVisibility() == View.INVISIBLE) fab.show();
-    }
-
-    public static void hideFab(FloatingActionButton fab) {
-        if (fab.getVisibility() != View.VISIBLE) return;
-        fab.hide(new FloatingActionButton.OnVisibilityChangedListener() {
-            @Override
-            public void onHidden(FloatingActionButton fab) {
-                fab.setVisibility(View.INVISIBLE);
-            }
-        });
     }
 
     public static boolean hasPIP() {
