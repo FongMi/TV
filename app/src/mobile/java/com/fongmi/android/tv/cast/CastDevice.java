e5c157472e9c2020fc32f34b1109d804c82cbc5c
FongMi <b8892778@gmail.com>
2023-04-17 22:53:51 +0800

    Upload missing files

---
+++ app/src/mobile/java/com/fongmi/android/tv/cast/CastDevice.java
@@ -0,0 +1,57 @@
+package com.fongmi.android.tv.cast;
+
+import org.fourthline.cling.model.meta.Device;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CastDevice {
+
+    private final List<Device<?, ?, ?>> devices;
+
+    private static class Loader {
+        static volatile CastDevice INSTANCE = new CastDevice();
+    }
+
+    public static CastDevice get() {
+        return Loader.INSTANCE;
+    }
+
+    public CastDevice() {
+        this.devices = new ArrayList<>();
+    }
+
+    public boolean isEmpty() {
+        return devices.isEmpty();
+    }
+
+    private com.fongmi.android.tv.bean.Device create(Device<?, ?, ?> item) {
+        com.fongmi.android.tv.bean.Device device = new com.fongmi.android.tv.bean.Device();
+        device.setUuid(item.getIdentity().getUdn().getIdentifierString());
+        device.setName(item.getDetails().getFriendlyName());
+        device.setType(2);
+        return device;
+    }
+
+    public List<com.fongmi.android.tv.bean.Device> getAll() {
+        List<com.fongmi.android.tv.bean.Device> items = new ArrayList<>();
+        for (Device<?, ?, ?> device : devices) items.add(create(device));
+        return items;
+    }
+
+    public List<com.fongmi.android.tv.bean.Device> add(Device<?, ?, ?> device) {
+        devices.remove(device);
+        devices.add(device);
+        return getAll();
+    }
+
+    public com.fongmi.android.tv.bean.Device remove(Device<?, ?, ?> device) {
+        devices.remove(device);
+        return create(device);
+    }
+
+    public Device<?, ?, ?> find(com.fongmi.android.tv.bean.Device item) {
+        for (Device<?, ?, ?> device : devices) if (device.getIdentity().getUdn().getIdentifierString().equals(item.getUuid())) return device;
+        return null;
+    }
+}
