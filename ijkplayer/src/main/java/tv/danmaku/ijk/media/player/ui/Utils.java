9ce8800776bd45ab52d040457b0f0fc2d957d8a5
FongMi <b8892778@gmail.com>
2023-04-18 22:08:29 +0800

    Set default ua

--- ijkplayer/src/main/java/tv/danmaku/ijk/media/player/ui/Utils.java
+++ ijkplayer/src/main/java/tv/danmaku/ijk/media/player/ui/Utils.java
@@ -9,6 +9,7 @@ import android.util.DisplayMetrics;
 public class Utils {
 
     public static final String USER_AGENT = "User-Agent";
+    public static final String CHROME = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36";
 
     public static float dp2px(Context context, float dpValue) {
         return Math.round((dpValue * context.getResources().getDisplayMetrics().densityDpi) / DisplayMetrics.DENSITY_DEFAULT);
