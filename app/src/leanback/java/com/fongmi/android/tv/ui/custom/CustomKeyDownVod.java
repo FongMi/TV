c07f9329002e9920b5265815539274ba41b23430
FongMi <b8892778@gmail.com>
2023-04-19 22:18:11 +0800

    [mobile] adjust live control

--- app/src/leanback/java/com/fongmi/android/tv/ui/custom/CustomKeyDownVod.java
+++ app/src/leanback/java/com/fongmi/android/tv/ui/custom/CustomKeyDownVod.java
@@ -131,11 +131,8 @@ public class CustomKeyDownVod extends GestureDetector.SimpleOnGestureListener {
 
     private void checkSide(MotionEvent e2) {
         int half = ResUtil.getScreenWidthNav() / 2;
-        if (e2.getX() > half) {
-            changeVolume = true;
-        } else {
-            changeBright = true;
-        }
+        if (e2.getX() > half) changeVolume = true;
+        else changeBright = true;
     }
 
     private void setBright(float deltaY) {
