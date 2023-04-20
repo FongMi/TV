fb1bdd9c59b1cc2a9ea3e2a3b2dd3158226e170c
FongMi <b8892778@gmail.com>
2023-04-20 10:27:25 +0800

    Fix build

--- app/src/main/java/com/fongmi/android/tv/net/OkHttp.java
+++ app/src/main/java/com/fongmi/android/tv/net/OkHttp.java
@@ -12,6 +12,7 @@ import okhttp3.Headers;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 
 public class OkHttp {
 
@@ -38,12 +39,24 @@ public class OkHttp {
         return client().newCall(new Request.Builder().url(url).build());
     }
 
+    public static Call newCall(OkHttpClient client, String url) {
+        return client.newCall(new Request.Builder().url(url).build());
+    }
+
     public static Call newCall(String url, Headers headers) {
         return client().newCall(new Request.Builder().url(url).headers(headers).build());
     }
 
     public static Call newCall(String url, LinkedHashMap<String, String> params) {
         return client().newCall(new Request.Builder().url(buildUrl(url, params)).build());
+    }
+
+    public static Call newCall(OkHttpClient client, String url, LinkedHashMap<String, String> params) {
+        return client.newCall(new Request.Builder().url(buildUrl(url, params)).build());
+    }
+
+    public static Call newCall(OkHttpClient client, String url, RequestBody body) {
+        return client.newCall(new Request.Builder().url(url).post(body).build());
     }
 
     private static HttpUrl buildUrl(String url, LinkedHashMap<String, String> params) {
