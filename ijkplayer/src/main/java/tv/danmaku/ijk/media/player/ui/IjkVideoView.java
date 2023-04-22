1d57422952f863a9749df1cdecdd36168c56521c
FongMi <b8892778@gmail.com>
2023-04-22 12:35:10 +0800

    Revert ua

--- ijkplayer/src/main/java/tv/danmaku/ijk/media/player/ui/IjkVideoView.java
+++ ijkplayer/src/main/java/tv/danmaku/ijk/media/player/ui/IjkVideoView.java
@@ -176,7 +176,7 @@ public class IjkVideoView extends FrameLayout implements MediaController.MediaPl
     }
 
     private void fixUserAgent() {
-        if (!mHeaders.containsKey(Utils.USER_AGENT)) mHeaders.put(Utils.USER_AGENT, Utils.CHROME);
+        if (!mHeaders.containsKey(Utils.USER_AGENT)) mHeaders.put(Utils.USER_AGENT, Utils.getUserAgent(mAppContext));
         mIjkPlayer.setOption(format, "user_agent", mHeaders.get(Utils.USER_AGENT));
         mHeaders.remove(Utils.USER_AGENT);
     }
