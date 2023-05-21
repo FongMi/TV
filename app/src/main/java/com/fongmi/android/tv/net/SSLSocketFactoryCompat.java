88f1c070cd258b02a5352ca081297e08afe5f138
FongMi <b8892778@gmail.com>
2023-05-21 21:52:24 +0800

    Fix build

--- app/src/main/java/com/fongmi/android/tv/net/SSLSocketFactoryCompat.java
+++ app/src/main/java/com/fongmi/android/tv/net/SSLSocketFactoryCompat.java
@@ -22,6 +22,7 @@ public class SSLSocketFactoryCompat extends SSLSocketFactory {
     public static final HostnameVerifier hostnameVerifier = (hostname, session) -> true;
 
     public static final X509TrustManager trustAllCert = new X509TrustManager() {
+
         @Override
         public void checkClientTrusted(X509Certificate[] chain, String authType) {
         }
