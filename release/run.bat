4e6ee4bd42ec92f8ff034de7734504ab3185e7de
FongMi <b8892778@gmail.com>
2023-05-23 18:54:51 +0800

    [leanback] update dev

---
+++ release/run.bat
@@ -0,0 +1,12 @@
+move .\leanbackJavaArm64_v8a\release\*.apk .\
+move .\leanbackJavaArmeabi_v7a\release\*.apk .\
+move .\leanbackPythonArm64_v8a\release\*.apk .\
+move .\leanbackPythonArmeabi_v7a\release\*.apk .\
+move .\mobileJava\release\*.apk .\
+move .\mobilePython\release\*.apk .\
+rmdir /s /q .\leanbackJavaArm64_v8a
+rmdir /s /q .\leanbackJavaArmeabi_v7a
+rmdir /s /q .\leanbackPythonArm64_v8a
+rmdir /s /q .\leanbackPythonArmeabi_v7a
+rmdir /s /q .\mobileJava
+rmdir /s /q .\mobilePython
