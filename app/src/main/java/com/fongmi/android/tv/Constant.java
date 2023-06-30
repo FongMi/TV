39577129ec54e6656f1e096eb4e401df735195e0
FongMi <b8892778@gmail.com>
2023-06-30 13:31:14 +0800

    Clean code

--- app/src/main/java/com/fongmi/android/tv/Constant.java
+++ app/src/main/java/com/fongmi/android/tv/Constant.java
@@ -9,19 +9,19 @@ public class Constant {
     public static final int INTERVAL_TRAFFIC = 500;
     //點播爬蟲時間
     public static final int TIMEOUT_VOD = 30 * 1000;
-    //直播解析時間
-    public static final int TIMEOUT_LIVE = 10 * 1000;
+    //直播爬蟲時間
+    public static final int TIMEOUT_LIVE = 30 * 1000;
     //播放超時時間
     public static final int TIMEOUT_PLAY = 15 * 1000;
     //解析預設時間
     public static final int TIMEOUT_PARSE_DEF = 15 * 1000;
     //嗅探超時時間
     public static final int TIMEOUT_PARSE_WEB = 15 * 1000;
-    //網路超時時間
-    public static final int TIMEOUT_HTTP = 30 * 1000;
+    //直播解析時間
+    public static final int TIMEOUT_PARSE_LIVE = 10 * 1000;
     //代理超時時間
     public static final int TIMEOUT_GITHUB = 5 * 1000;
-    //代理超時時間
+    //同步超時時間
     public static final int TIMEOUT_SYNC = 2 * 1000;
     //搜尋線程數量
     public static final int THREAD_POOL = 5;
