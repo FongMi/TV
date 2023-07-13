a578fc695e3e024d991c5dfa6c5c9265f1cae5f1
FongMi <b8892778@gmail.com>
2023-07-13 17:41:02 +0800

    js support load jar

--- app/src/main/java/com/fongmi/android/tv/api/JsLoader.java
+++ app/src/main/java/com/fongmi/android/tv/api/JsLoader.java
@@ -1,11 +1,17 @@
 package com.fongmi.android.tv.api;
 
 import com.fongmi.android.tv.App;
+import com.fongmi.android.tv.utils.FileUtil;
+import com.fongmi.android.tv.utils.Utils;
 import com.github.catvod.crawler.Spider;
 import com.github.catvod.crawler.SpiderNull;
+import com.github.catvod.net.OkHttp;
 
+import java.io.File;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+
+import dalvik.system.DexClassLoader;
 
 public class JsLoader {
 
@@ -14,7 +20,6 @@ public class JsLoader {
 
     public JsLoader() {
         spiders = new ConcurrentHashMap<>();
-        init();
     }
 
     public void clear() {
@@ -26,14 +31,45 @@ public class JsLoader {
         this.recent = recent;
     }
 
-    private void init() {
-
+    private File download(String jar) {
+        try {
+            return FileUtil.write(FileUtil.getJar(jar), OkHttp.newCall(jar).execute().body().bytes());
+        } catch (Exception e) {
+            return FileUtil.getJar(jar);
+        }
     }
 
-    public Spider getSpider(String key, String api, String ext) {
+    private File getFile(String jar) {
+        String[] texts = jar.split(";md5;");
+        String md5 = !jar.startsWith("file") && texts.length > 1 ? texts[1].trim() : "";
+        jar = texts[0];
+        if (jar.startsWith("img+")) {
+            return Decoder.getSpider(jar, md5);
+        } else if (md5.length() > 0 && FileUtil.equals(jar, md5)) {
+            return FileUtil.getJar(jar);
+        } else if (jar.startsWith("http")) {
+            return download(jar);
+        } else if (jar.startsWith("file")) {
+            return FileUtil.getLocal(jar);
+        } else if (!jar.isEmpty()) {
+            return getFile(Utils.convert(ApiConfig.getUrl(), jar));
+        } else {
+            return null;
+        }
+    }
+
+    private DexClassLoader dex(String jar) {
+        try {
+            return jar.isEmpty() ? null : new DexClassLoader(getFile(jar).getAbsolutePath(), FileUtil.getCachePath(), null, App.get().getClassLoader());
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public Spider getSpider(String key, String api, String ext, String jar) {
         try {
             if (spiders.containsKey(key)) return spiders.get(key);
-            Spider spider = new com.hiker.drpy.Spider(api);
+            Spider spider = new com.hiker.drpy.Spider(api, dex(jar));
             spider.init(App.get(), ext);
             spiders.put(key, spider);
             return spider;
