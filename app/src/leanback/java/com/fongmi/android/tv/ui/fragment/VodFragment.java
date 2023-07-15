4a0be7f6b6d15bab6599022302132857bbba851f
FongMi <b8892778@gmail.com>
2023-07-15 09:49:52 +0800

    [leanback] optimize folder ux

--- app/src/leanback/java/com/fongmi/android/tv/ui/fragment/VodFragment.java
+++ app/src/leanback/java/com/fongmi/android/tv/ui/fragment/VodFragment.java
@@ -242,10 +242,14 @@ public class VodFragment extends BaseFragment implements CustomScroller.Callback
 
     @Override
     public void onItemClick(Vod item) {
-        if (item.isFolder()) mBinding.recycler.setMoveTop(false);
-        if (item.isFolder()) mPages.add(Page.get(item.getVodId(), mBinding.recycler.getSelectedPosition()));
-        if (item.isFolder()) getVideo(item.getVodId(), "1");
-        else DetailActivity.start(getActivity(), getKey(), item.getVodId(), item.getVodName());
+        if (item.isFolder()) {
+            mPages.add(Page.get(item.getVodId(), mBinding.recycler.getSelectedPosition()));
+            mBinding.recycler.setMoveTop(false);
+            getVideo(item.getVodId(), "1");
+        } else {
+            if (!isFolder()) DetailActivity.start(getActivity(), getKey(), item.getVodId(), item.getVodName());
+            else DetailActivity.start(getActivity(), getKey(), item.getVodId(), item.getVodName(), item.getVodName());
+        }
     }
 
     @Override
