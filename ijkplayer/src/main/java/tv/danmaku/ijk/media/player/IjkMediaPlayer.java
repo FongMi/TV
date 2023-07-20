c9410e3cbd502ecdecc664b7beddfc82541721e8
FongMi <b8892778@gmail.com>
2023-07-20 12:55:18 +0800

    Update IjkMediaPlayer.java

--- ijkplayer/src/main/java/tv/danmaku/ijk/media/player/IjkMediaPlayer.java
+++ ijkplayer/src/main/java/tv/danmaku/ijk/media/player/IjkMediaPlayer.java
@@ -359,11 +359,11 @@ public final class IjkMediaPlayer extends AbstractMediaPlayer {
         StringBuilder sb = new StringBuilder();
         for (Map.Entry<String, String> entry : headers.entrySet()) {
             String value = entry.getValue();
-            sb.append(entry.getKey()).append(": ");
+            sb.append(entry.getKey()).append(":");
             if (!TextUtils.isEmpty(value)) sb.append(value);
             sb.append("\r\n");
+            setOption(OPT_CATEGORY_FORMAT, "headers", sb.toString());
         }
-        setOption(OPT_CATEGORY_FORMAT, "headers", sb.toString());
         setDataSource(path);
     }
 
