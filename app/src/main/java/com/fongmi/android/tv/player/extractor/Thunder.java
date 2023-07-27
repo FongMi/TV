03b10c74662c2ed3c8da6efbc9b8f9c52c66c767
FongMi <b8892778@gmail.com>
2023-07-27 14:51:27 +0800

    Support ed2k and ftp

--- app/src/main/java/com/fongmi/android/tv/player/extractor/Thunder.java
+++ app/src/main/java/com/fongmi/android/tv/player/extractor/Thunder.java
@@ -1,34 +1,57 @@
 package com.fongmi.android.tv.player.extractor;
 
+import android.net.Uri;
+import android.os.SystemClock;
+
 import com.fongmi.android.tv.player.Source;
+import com.github.catvod.utils.Path;
+import com.github.catvod.utils.Util;
+import com.xunlei.downloadlib.XLTaskHelper;
+import com.xunlei.downloadlib.parameter.GetTaskId;
+
+import java.io.File;
+import java.util.Objects;
 
 public class Thunder implements Source.Extractor {
 
-    public Thunder() {
-    }
+    private GetTaskId taskId;
 
     @Override
     public boolean match(String scheme, String host) {
-        return scheme.equalsIgnoreCase("ed2k") || scheme.equalsIgnoreCase("ftp") || scheme.equalsIgnoreCase("thunder") || scheme.equalsIgnoreCase("magnet");
+        return scheme.equals("ed2k") || scheme.equals("ftp") || scheme.equals("torrent");
     }
 
     @Override
     public String fetch(String url) throws Exception {
-        return url;
+        Uri uri = Uri.parse(url);
+        boolean torrent = "torrent".equals(uri.getScheme());
+        return torrent ? fetchTorrent(uri) : fetchThunder(url);
+    }
+
+    private String fetchTorrent(Uri uri) {
+        File torrent = new File(uri.getPath());
+        String name = uri.getQueryParameter("name");
+        int index = Integer.parseInt(uri.getQueryParameter("index"));
+        taskId = XLTaskHelper.get().addTorrentTask(torrent, Objects.requireNonNull(torrent.getParentFile()), index);
+        while (XLTaskHelper.get().getBtSubTaskInfo(taskId, index).mTaskInfo.mTaskStatus == 0) SystemClock.sleep(10);
+        return XLTaskHelper.get().getLocalUrl(new File(torrent.getParent(), name));
+    }
+
+    private String fetchThunder(String url) {
+        File folder = Path.thunder(Util.md5(url));
+        taskId = XLTaskHelper.get().addThunderTask(url, folder);
+        return XLTaskHelper.get().getLocalUrl(taskId.getSaveFile());
     }
 
     @Override
     public void stop() {
-
+        if (taskId == null) return;
+        XLTaskHelper.get().deleteTask(taskId);
+        taskId = null;
     }
 
     @Override
-    public void destroy() {
-
-    }
-
-    @Override
-    public void release() {
-
+    public void exit() {
+        XLTaskHelper.get().release();
     }
 }
--- thunder/src/main/java/com/xunlei/downloadlib/parameter/GetTaskId.java
+++ thunder/src/main/java/com/xunlei/downloadlib/parameter/GetTaskId.java
@@ -1,19 +1,42 @@
 package com.xunlei.downloadlib.parameter;
 
+import java.io.File;
+
 public class GetTaskId {
 
     private long mTaskId;
-    private String mSavePath;
+    private File mSavePath;
+    private String mFileName;
+
+    public GetTaskId() {
+    }
+
+    public GetTaskId(File savePath) {
+        this.mSavePath = savePath;
+    }
+
+    public GetTaskId(File savePath, String fileName) {
+        this.mSavePath = savePath;
+        this.mFileName = fileName;
+    }
 
     public long getTaskId() {
         return this.mTaskId;
     }
 
-    public void setSavePath(String savePath) {
-        this.mSavePath = savePath;
+    public File getSavePath() {
+        return mSavePath;
     }
 
-    public String getSavePath() {
-        return mSavePath;
+    public String getFileName() {
+        return mFileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.mFileName = fileName;
+    }
+
+    public File getSaveFile() {
+        return new File(getSavePath(), getFileName());
     }
 }
