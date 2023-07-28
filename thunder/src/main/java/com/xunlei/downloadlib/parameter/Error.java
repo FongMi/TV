2b6ab6962b5288e7c0493a106ef7cd6b2d8e6572
FongMi <b8892778@gmail.com>
2023-07-29 00:31:06 +0800

    Update Error.java

--- thunder/src/main/java/com/xunlei/downloadlib/parameter/Error.java
+++ thunder/src/main/java/com/xunlei/downloadlib/parameter/Error.java
@@ -5,17 +5,11 @@ public class Error {
     public static String get(int code) {
         switch (code) {
             case 9125:
+            case 111120:
                 return "檔案名稱太長";
             case 9301:
-                return "緩衝區不足";
             case 111085:
-                return "硬碟空間不足";
-            case 111120:
-                return "檔案路徑太長";
-            case 111142:
-                return "檔案大小太小";
-            case 111171:
-                return "網路連線失敗";
+                return "儲存空間不足";
             case 9304:
             case 114001:
             case 114004:
@@ -24,7 +18,7 @@ public class Error {
             case 114007:
             case 114011:
             case 111154:
-                return "版權限制：無法下載";
+                return "版權限制";
             case 114101:
                 return "地址失效";
             default:
