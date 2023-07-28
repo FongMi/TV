f7a8b59701a3191ba72589d0fda541fa2908cf12
FongMi <b8892778@gmail.com>
2023-07-28 10:45:15 +0800

    Add logger and clean code

--- catvod/src/main/java/com/github/catvod/crawler/SpiderDebug.java
+++ catvod/src/main/java/com/github/catvod/crawler/SpiderDebug.java
@@ -1,16 +1,20 @@
 package com.github.catvod.crawler;
 
-import android.util.Log;
+import android.text.TextUtils;
+
+import com.orhanobut.logger.Logger;
 
 public class SpiderDebug {
 
     private static final String TAG = SpiderDebug.class.getSimpleName();
 
     public static void log(Throwable th) {
-        Log.d(TAG, th.getMessage());
+        if (th == null || TextUtils.isEmpty(th.getMessage())) return;
+        Logger.t(TAG).d(th.getMessage());
     }
 
     public static void log(String msg) {
-        Log.d(TAG, msg);
+        if (TextUtils.isEmpty(msg)) return;
+        Logger.t(TAG).d(msg);
     }
 }
