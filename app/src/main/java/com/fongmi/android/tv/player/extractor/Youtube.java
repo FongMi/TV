c56814369a557c0e8299688e31ac0415e2dfbfb1
FongMi <b8892778@gmail.com>
2023-07-28 11:27:21 +0800

    Clean code

--- app/src/main/java/com/fongmi/android/tv/player/extractor/Youtube.java
+++ app/src/main/java/com/fongmi/android/tv/player/extractor/Youtube.java
@@ -1,9 +1,18 @@
 package com.fongmi.android.tv.player.extractor;
 
+import com.fongmi.android.tv.impl.NewPipeImpl;
 import com.fongmi.android.tv.player.Source;
 import com.fongmi.android.tv.utils.Sniffer;
 import com.github.catvod.net.OkHttp;
 import com.google.common.net.HttpHeaders;
+
+import org.schabi.newpipe.extractor.NewPipe;
+import org.schabi.newpipe.extractor.ServiceList;
+import org.schabi.newpipe.extractor.exceptions.ExtractionException;
+import org.schabi.newpipe.extractor.linkhandler.LinkHandler;
+import org.schabi.newpipe.extractor.services.youtube.extractors.YoutubeStreamExtractor;
+import org.schabi.newpipe.extractor.services.youtube.linkHandler.YoutubeStreamLinkHandlerFactory;
+import org.schabi.newpipe.extractor.stream.VideoStream;
 
 import java.util.Arrays;
 import java.util.List;
@@ -16,31 +25,44 @@ public class Youtube implements Source.Extractor {
 
     @Override
     public boolean match(String scheme, String host) {
-        return host.contains("youtube.com");
+        return host.contains("youtube.com") || host.contains("youtu.be");
+    }
+
+    public Youtube() {
+        NewPipe.init(new NewPipeImpl());
     }
 
     @Override
     public String fetch(String url) throws Exception {
-        String result = OkHttp.newCall(url, Headers.of(HttpHeaders.USER_AGENT, Sniffer.CHROME)).execute().body().string();
-        Pattern pattern = Pattern.compile("hlsManifestUrl\\S*?(https\\S*?\\.m3u8)");
-        Matcher matcher = pattern.matcher(result);
-        if (!matcher.find()) return "";
-        String stable = matcher.group(1);
-        result = OkHttp.newCall(stable, Headers.of(HttpHeaders.USER_AGENT, Sniffer.CHROME)).execute().body().string();
-        String quality = find(result);
-        return quality.isEmpty() ? url : quality;
+        String html = OkHttp.newCall(url, Headers.of(HttpHeaders.USER_AGENT, Sniffer.CHROME)).execute().body().string();
+        Matcher matcher = Pattern.compile("hlsManifestUrl\\S*?(https\\S*?\\.m3u8)").matcher(html);
+        if (matcher.find()) {
+            html = OkHttp.newCall(matcher.group(1), Headers.of(HttpHeaders.USER_AGENT, Sniffer.CHROME)).execute().body().string();
+            return find(html);
+        } else {
+            LinkHandler handler = YoutubeStreamLinkHandlerFactory.getInstance().fromUrl(url);
+            YoutubeStreamExtractor extractor = new YoutubeStreamExtractor(ServiceList.YouTube, handler);
+            extractor.fetchPage();
+            return find(extractor);
+        }
     }
 
-    private String find(String result) {
+    private String find(YoutubeStreamExtractor extractor) throws ExtractionException {
+        VideoStream item = extractor.getVideoStreams().get(0);
+        for (VideoStream stream : extractor.getVideoStreams()) if (!stream.isVideoOnly() && stream.getHeight() >= item.getHeight()) item = stream;
+        return item.getContent();
+    }
+
+    private String find(String html) {
         String url = "";
         List<String> items = Arrays.asList("301", "300", "96", "95", "94");
-        for (String item : items) if (!(url = find(result, "https:/.*/" + item + "/.*index.m3u8")).isEmpty()) break;
+        for (String item : items) if (!(url = find(html, "https:/.*/" + item + "/.*index.m3u8")).isEmpty()) break;
         return url;
     }
 
-    private String find(String result, String rule) {
+    private String find(String html, String rule) {
         Pattern pattern = Pattern.compile(rule);
-        Matcher matcher = pattern.matcher(result);
+        Matcher matcher = pattern.matcher(html);
         if (matcher.find()) return matcher.group();
         return "";
     }
@@ -50,10 +72,6 @@ public class Youtube implements Source.Extractor {
     }
 
     @Override
-    public void destroy() {
-    }
-
-    @Override
-    public void release() {
+    public void exit() {
     }
 }
