8e6cdc2ea602ebb7d025c5838c23887fee58f99a
FongMi <b8892778@gmail.com>
2023-07-30 18:02:10 +0800

    Clean code

--- ijkplayer/src/main/java/tv/danmaku/ijk/media/player/IjkTimedText.java
+++ ijkplayer/src/main/java/tv/danmaku/ijk/media/player/IjkTimedText.java
@@ -2,17 +2,30 @@ package tv.danmaku.ijk.media.player;
 
 public final class IjkTimedText {
 
-    private final String text;
+    private String text;
+    private int[] bitmap;
 
     public static IjkTimedText create(String text) {
         return new IjkTimedText(text);
+    }
+
+    public static IjkTimedText create(int[] bitmap) {
+        return new IjkTimedText(bitmap);
     }
 
     public IjkTimedText(String text) {
         this.text = text;
     }
 
+    public IjkTimedText(int[] bitmap) {
+        this.bitmap = bitmap;
+    }
+
     public String getText() {
         return text;
+    }
+
+    public int[] getBitmap() {
+        return bitmap;
     }
 }
