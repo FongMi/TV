3ab8a0fc6024bcc9fd2c2417fffaecc8dced4381
FongMi <b8892778@gmail.com>
2023-08-11 08:49:42 +0800

    Update cleaner.bat

--- other/tools/cleaner.bat
+++ other/tools/cleaner.bat
@@ -1,5 +1,10 @@
 git clone --mirror https://github.com/FongMi/TV.git
 java -jar bfg.jar --delete-files *.apk TV.git
+java -jar bfg.jar --delete-files libp*.so TV.git
+java -jar bfg.jar --delete-files libjpa*.so TV.git
+java -jar bfg.jar --delete-files libxl*.so TV.git
+java -jar bfg.jar --delete-files libmitv.so TV.git
+java -jar bfg.jar --delete-files libtvcore.so TV.git
 cd TV.git
 git reflog expire --expire=now --all && git gc --prune=now --aggressive
 git push
