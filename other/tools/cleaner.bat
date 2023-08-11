364b439695aa802addbc91a26ec7a77eed639b1e
FongMi <b8892778@gmail.com>
2023-08-11 08:52:57 +0800

    Update cleaner.bat

--- other/tools/cleaner.bat
+++ other/tools/cleaner.bat
@@ -1,8 +1,9 @@
 git clone --mirror https://github.com/FongMi/TV.git
 java -jar bfg.jar --delete-files *.apk TV.git
-java -jar bfg.jar --delete-files libp*.so TV.git
-java -jar bfg.jar --delete-files libjpa*.so TV.git
+java -jar bfg.jar --delete-files libp*p.so TV.git
 java -jar bfg.jar --delete-files libxl*.so TV.git
+java -jar bfg.jar --delete-files libjpa*.so TV.git
+java -jar bfg.jar --delete-files libp2p*.so TV.git
 java -jar bfg.jar --delete-files libmitv.so TV.git
 java -jar bfg.jar --delete-files libtvcore.so TV.git
 cd TV.git
