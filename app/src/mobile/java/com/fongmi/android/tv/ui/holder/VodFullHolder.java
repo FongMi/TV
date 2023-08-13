3017af91a492864eb6afc7d946652495bf25e82e
FongMi <b8892778@gmail.com>
2023-08-13 19:11:58 +0800

    Revert full image

--- app/src/mobile/java/com/fongmi/android/tv/ui/holder/VodFullHolder.java
+++ app/src/mobile/java/com/fongmi/android/tv/ui/holder/VodFullHolder.java
@@ -1,18 +1,9 @@
 package com.fongmi.android.tv.ui.holder;
 
-import android.graphics.drawable.Drawable;
-import android.widget.ImageView;
+import android.view.View;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.DataSource;
-import com.bumptech.glide.load.engine.GlideException;
-import com.bumptech.glide.request.RequestListener;
-import com.bumptech.glide.request.target.Target;
-import com.fongmi.android.tv.App;
-import com.fongmi.android.tv.R;
 import com.fongmi.android.tv.bean.Vod;
 import com.fongmi.android.tv.databinding.AdapterVodFullBinding;
 import com.fongmi.android.tv.ui.base.BaseVodHolder;
@@ -29,22 +20,7 @@ public class VodFullHolder extends BaseVodHolder {
 
     @Override
     public void initView(Vod item) {
-        Glide.with(App.get()).load(ImgUtil.getUrl(item.getVodPic())).dontAnimate().listener(getListener(binding.image)).into(binding.image);
-    }
-
-    private RequestListener<Drawable> getListener(ImageView view) {
-        return new RequestListener<>() {
-            @Override
-            public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
-                view.setScaleType(ImageView.ScaleType.CENTER);
-                view.setImageResource(R.drawable.ic_img_error);
-                return true;
-            }
-
-            @Override
-            public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
-                return false;
-            }
-        };
+        ImgUtil.load(item.getVodPic(), binding.image);
+        binding.image.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
     }
 }
