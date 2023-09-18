59791408daf4185219e4c5ab6f13260c69dddf43
FongMi <b8892778@gmail.com>
2023-09-18 17:52:57 +0800

    Update log

--- app/src/main/java/com/fongmi/android/tv/utils/Sniffer.java
+++ app/src/main/java/com/fongmi/android/tv/utils/Sniffer.java
@@ -5,8 +5,8 @@ import android.text.TextUtils;
 
 import com.fongmi.android.tv.api.ApiConfig;
 import com.fongmi.android.tv.bean.Rule;
-import com.github.catvod.crawler.SpiderDebug;
 import com.github.catvod.utils.Util;
+import com.orhanobut.logger.Logger;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -17,6 +17,8 @@ import java.util.Map;
 import java.util.regex.Pattern;
 
 public class Sniffer {
+
+    private static final String TAG = Sniffer.class.getSimpleName();
 
     public static final String CHROME = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36";
     public static final Pattern RULE = Pattern.compile("http((?!http).){12,}?\\.(m3u8|mp4|flv|avi|mkv|rm|wmv|mpg|m4a|mp3)\\?.*|http((?!http).){12,}\\.(m3u8|mp4|flv|avi|mkv|rm|wmv|mpg|m4a|mp3)|http((?!http).)*?video/tos*");
@@ -40,7 +42,7 @@ public class Sniffer {
     }
 
     public static boolean isVideoFormat(String url, Map<String, String> headers) {
-        SpiderDebug.log(url);
+        Logger.t(TAG).d(url);
         if (matchOrContain(url)) return true;
         if (headers.containsKey("Accept") && headers.get("Accept").startsWith("image")) return false;
         if (url.contains("url=http") || url.contains("v=http") || url.contains(".css") || url.contains(".html")) return false;
---
+++ quickjs/src/main/java/com/fongmi/quickjs/method/Console.java
@@ -0,0 +1,29 @@
+package com.fongmi.quickjs.method;
+
+import com.orhanobut.logger.Logger;
+import com.whl.quickjs.wrapper.QuickJSContext;
+
+public class Console implements QuickJSContext.Console {
+
+    private static final String TAG = Console.class.getSimpleName();
+
+    @Override
+    public void log(String info) {
+        Logger.t(TAG).d(info);
+    }
+
+    @Override
+    public void info(String info) {
+        Logger.t(TAG).i(info);
+    }
+
+    @Override
+    public void warn(String info) {
+        Logger.t(TAG).w(info);
+    }
+
+    @Override
+    public void error(String info) {
+        Logger.t(TAG).e(info);
+    }
+}
