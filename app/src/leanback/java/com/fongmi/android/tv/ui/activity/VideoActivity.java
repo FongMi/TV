79d0f7230e55b4dab271984e234948f98a811423
FongMi <b8892778@gmail.com>
2023-10-02 16:52:15 +0800

    [leanback] adjust focus

--- app/src/leanback/java/com/fongmi/android/tv/ui/activity/VideoActivity.java
+++ app/src/leanback/java/com/fongmi/android/tv/ui/activity/VideoActivity.java
@@ -493,7 +493,6 @@ public class VideoActivity extends BaseActivity implements CustomKeyDownVod.List
         setText(mBinding.director, R.string.detail_director, Html.fromHtml(item.getVodDirector()).toString());
         mFlagAdapter.setItems(item.getVodFlags(), null);
         mBinding.content.setMaxLines(getMaxLines());
-        mBinding.video.requestFocus();
         setArtwork(item.getVodPic());
         getPart(item.getVodName());
         App.removeCallbacks(mR3);
@@ -522,7 +521,6 @@ public class VideoActivity extends BaseActivity implements CustomKeyDownVod.List
         mBinding.flag.setSelectedPosition(mFlagAdapter.indexOf(item));
         notifyItemChanged(mBinding.flag, mFlagAdapter);
         setEpisodeAdapter(item.getEpisodes());
-        mBinding.episode.requestFocus();
         setQualityVisible(false);
         seamless(item, force);
     }
@@ -535,6 +533,7 @@ public class VideoActivity extends BaseActivity implements CustomKeyDownVod.List
     }
 
     private void seamless(Flag flag, boolean force) {
+        if (mEpisodeAdapter.size() > 0) mBinding.episode.requestFocus();
         if (Setting.getFlag() == 1 && (mHistory.isNew() || !force)) return;
         Episode episode = flag.find(mHistory.getVodRemarks(), getMark().isEmpty());
         setQualityVisible(episode != null && episode.isActivated() && mQualityAdapter.getItemCount() > 1);
