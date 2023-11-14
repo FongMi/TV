d073bd475d29ebb73236a0bbb86185e3f3dd83dd
FongMi <b8892778@gmail.com>
2023-11-15 01:15:18 +0800

    Rename so

--- jianpian/src/main/java/com/p2p/P2PClass.java
+++ jianpian/src/main/java/com/p2p/P2PClass.java
@@ -8,7 +8,7 @@ public class P2PClass {
     public int port;
 
     public P2PClass() {
-        System.load(Github.getSo("jpa_" + BuildConfig.FLAVOR));
+        System.load(Github.getSo("jpa"));
         this.port = P2Pdoxstarthttpd("TEST3E63BAAECDAA79BEAA91853490A69F08".getBytes(), Path.jpa().getAbsolutePath().getBytes());
     }
 
