f2997b344c10a30cbdeeb378fa5e891af485e3b0
FongMi <b8892778@gmail.com>
2023-11-23 13:48:12 +0800

    Fix crash

--- app/src/main/java/com/fongmi/android/tv/player/Players.java
+++ app/src/main/java/com/fongmi/android/tv/player/Players.java
@@ -157,7 +157,7 @@ public class Players implements Player.Listener, IMediaPlayer.Listener, Analytic
     }
 
     public Map<String, String> getHeaders() {
-        return checkUa(headers);
+        return headers == null ? new HashMap<>() : checkUa(headers);
     }
 
     public String[] getHeaderArray() {
