6dfe2d44a40ef7f2df8c7c8b393c438064c7e490
FongMi <b8892778@gmail.com>
2023-12-07 10:37:26 +0800

    Fix crash

---
+++ app/src/mobile/java/com/fongmi/android/tv/ui/custom/CustomViewPager.java
@@ -0,0 +1,29 @@
+package com.fongmi.android.tv.ui.custom;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.viewpager.widget.ViewPager;
+
+public class CustomViewPager extends ViewPager {
+
+    public CustomViewPager(@NonNull Context context) {
+        super(context);
+    }
+
+    public CustomViewPager(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        try {
+            return super.onInterceptTouchEvent(ev);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
