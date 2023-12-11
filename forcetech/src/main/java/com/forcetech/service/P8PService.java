a1ba3c03e8a8261dd2b28bacd61d4e7afff71e67
FongMi <b8892778@gmail.com>
2023-12-11 15:12:55 +0800

    Revert p7~p9

---
+++ forcetech/src/main/java/com/forcetech/service/P8PService.java
@@ -0,0 +1,11 @@
+package com.forcetech.service;
+
+import com.forcetech.Util;
+
+public class P8PService extends PxPService {
+
+    @Override
+    public int getPort() {
+        return Util.P8P;
+    }
+}
