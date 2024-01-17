a0379589b33a5beda8e932b7c1308b0dc7b8146f
okjack <okjackcaptain@gmail.com>
2024-01-17 12:04:30 +0800

    danmu line

--- app/src/main/java/com/fongmi/android/tv/Setting.java
+++ app/src/main/java/com/fongmi/android/tv/Setting.java
@@ -266,7 +266,7 @@ public class Setting {
     }
 
     public static int getDanmuLine(int line) {
-        return Math.min(Math.max(Prefers.getInt("danmu_line"), line), 15);
+        return Math.min(Prefers.getInt("danmu_line", line), 15);
     }
 
     public static void putDanmuLine(int line) {
