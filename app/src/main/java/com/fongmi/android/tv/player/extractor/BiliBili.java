6a975865afe1f2efd7e926594b44b4baf7bec876
FongMi <b8892778@gmail.com>
2024-01-27 00:11:47 +0800

    Fix json problem

--- app/src/main/java/com/fongmi/android/tv/player/extractor/BiliBili.java
+++ app/src/main/java/com/fongmi/android/tv/player/extractor/BiliBili.java
@@ -4,9 +4,9 @@ import android.net.Uri;
 
 import com.fongmi.android.tv.player.Source;
 import com.github.catvod.net.OkHttp;
+import com.github.catvod.utils.Json;
 import com.github.catvod.utils.Util;
 import com.google.common.net.HttpHeaders;
-import com.google.gson.JsonParser;
 
 import okhttp3.Headers;
 
@@ -22,7 +22,7 @@ public class BiliBili implements Source.Extractor {
         String room = Uri.parse(url).getPath().replace("/", "");
         String api = String.format("https://api.live.bilibili.com/room/v1/Room/playUrl?cid=%s&qn=20000&platform=h5", room);
         String result = OkHttp.newCall(api, Headers.of(HttpHeaders.USER_AGENT, Util.CHROME)).execute().body().string();
-        return JsonParser.parseString(result).getAsJsonObject().get("data").getAsJsonObject().get("durl").getAsJsonArray().get(0).getAsJsonObject().get("url").getAsString();
+        return Json.parse(result).getAsJsonObject().get("data").getAsJsonObject().get("durl").getAsJsonArray().get(0).getAsJsonObject().get("url").getAsString();
     }
 
     @Override
