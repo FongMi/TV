20eb50ca155f1279d3f7397aa512c84610f7df21
FongMi <b8892778@gmail.com>
2024-02-02 14:50:04 +0800

    Update CustomRecyclerView.java

--- app/src/main/java/com/fongmi/android/tv/ui/custom/CustomRecyclerView.java
+++ app/src/main/java/com/fongmi/android/tv/ui/custom/CustomRecyclerView.java
@@ -5,7 +5,6 @@ import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
