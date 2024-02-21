dc40bda71430135a9bdd313580938f8f10f3c1ce
FongMi <b8892778@gmail.com>
2024-02-21 15:52:07 +0800

    Update Shell.java

--- catvod/src/main/java/com/github/catvod/utils/Shell.java
+++ catvod/src/main/java/com/github/catvod/utils/Shell.java
@@ -4,10 +4,12 @@ import com.orhanobut.logger.Logger;
 
 public class Shell {
 
+    private static final String TAG = Shell.class.getSimpleName();
+
     public static void exec(String command) {
         try {
             int code = Runtime.getRuntime().exec(command).waitFor();
-            if (code != 0) Logger.e("Shell command failed with exit code " + code);
+            if (code != 0) Logger.t(TAG).d("Shell command '%s' failed with exit code '%s'", command, code);
         } catch (Exception e) {
             e.printStackTrace();
         }
