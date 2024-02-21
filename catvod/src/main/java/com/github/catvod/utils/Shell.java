89d110cc04cc539d84f116f005f371e1234bcf41
FongMi <b8892778@gmail.com>
2024-02-21 15:49:07 +0800

    Clean code

--- catvod/src/main/java/com/github/catvod/utils/Shell.java
+++ catvod/src/main/java/com/github/catvod/utils/Shell.java
@@ -1,11 +1,13 @@
 package com.github.catvod.utils;
 
+import com.orhanobut.logger.Logger;
+
 public class Shell {
 
     public static void exec(String command) {
         try {
             int code = Runtime.getRuntime().exec(command).waitFor();
-            if (code != 0) throw new RuntimeException("Shell command failed with exit code " + code);
+            if (code != 0) Logger.e("Shell command failed with exit code " + code);
         } catch (Exception e) {
             e.printStackTrace();
         }
