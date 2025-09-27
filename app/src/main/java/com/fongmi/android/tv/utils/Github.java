8e994ebe8c176d5fdd01c02fbee91fd993dca5f6
guang518 <35475020+guang518@users.noreply.github.com>
2025-09-28 02:50:47 +0800

    Update Github.java
    更新日志信息指向gongdian.top/fongmi
    下载链接指向gongdian.top/fongmi/download，经nginx跳转到网盘链接
    
    Signed-off-by: guang518 <35475020+guang518@users.noreply.github.com>

--- app/src/main/java/com/fongmi/android/tv/utils/Github.java
+++ app/src/main/java/com/fongmi/android/tv/utils/Github.java
@@ -4,6 +4,14 @@ public class Github {
 
     public static final String URL = "https://gongdian.top/fongmi";
 
+    // 添加更新链接常量
+    public static final String UPDATE_REDIRECT_URL = "https://gongdian.top/fongmi/download";
+
+    // 获取更新重定向链接
+    public static String getUpdateRedirectUrl() {
+        return UPDATE_REDIRECT_URL;
+    }
+
     private static String getUrl(String path, String name) {
         return URL + "/" + path + "/" + name;
     }
@@ -16,3 +24,4 @@ public class Github {
         return getUrl("apk/" + (dev ? "dev" : "release"), name + ".apk");
     }
 }
+
