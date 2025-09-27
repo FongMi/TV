4ac42c454f4b645dc2549491bf8337ec2ed9122c
guang518 <35475020+guang518@users.noreply.github.com>
2025-09-27 22:26:44 +0800

    Update Updater.java
    电视端启动更新，信息指向gongdian.top
    
    Signed-off-by: guang518 <35475020+guang518@users.noreply.github.com>

--- app/src/leanback/java/com/fongmi/android/tv/Updater.java
+++ app/src/leanback/java/com/fongmi/android/tv/Updater.java
@@ -69,9 +69,9 @@ public class Updater implements Download.Callback {
         return this;
     }
 
-    //cancel update
+    //enable update
     public void start(Activity activity) {
-        // App.execute(() -> doInBackground(activity));
+         App.execute(() -> doInBackground(activity));
     }
 
     private boolean need(int code, String name) {
